3ee03fee5514c4f015f24d0f95ddf1c4bca4983d
NeuTrix <neutrix@brandgeneering.com>
2018-01-11 14:38:33 -0800

    Linting

--- client/src/tests/componentsTests/TodoForm.test.js
+++ client/src/tests/componentsTests/TodoForm.test.js
@@ -1,29 +1,17 @@
+/* eslint-env node, mocha */
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// Testing...
-import { expect } from 'chai';
-
-import todoReducer from '../../reducers/todoReducer';
 import TodoList from '../../containers/TodoList';
-import { 
-	addTodo, 
-	removeTodo, 
-	toggleTodo 
-} from '../../actions/todoActions';
 
-// ====================================
-
+// ========= 
 
 // this may want a Provider to work
 describe('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(
-	  	<TodoList />
-  	, div
+		<TodoList />
+		, div
 	);
 
-	it('is ok', () => {
-		console.log('yes, you need a test or two here for refs');
-	});
 });
