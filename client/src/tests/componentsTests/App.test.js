37c82a67a481e54428cb9675585a2eb257472af0
NeuTrix <neutrix@brandgeneering.com>
2018-01-23 10:35:43 -0800

    Linting

--- client/src/tests/componentsTests/App.test.js
+++ client/src/tests/componentsTests/App.test.js
@@ -9,9 +9,9 @@ it('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(
 		<Provider store={ store } >
-  		<App />
-		</Provider>,
-  	div);
+			<App />
+		</Provider>
+		, div);
 });
 
 
