0273152eea0e4982bfbdcce1e7fbb361a49bf445
NeuTrix <neutrix@brandgeneering.com>
2018-02-06 13:57:28 -0800

    combined api reducer to root

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import todoReducer from './todoReducer';
+import apiReducer from './apiReducer';
 
 
 const todoApp = combineReducers({
 	// no need to provid a key/val pair if same names 
-	todos: todoReducer
+	todos: todoReducer,
+	apiTodos: apiReducer
 });
 
 export default todoApp;
