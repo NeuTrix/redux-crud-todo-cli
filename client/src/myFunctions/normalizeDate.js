0f05e61c05b32237e91a7b8ce46c4a056f042038
NeuTrix <neutrix@brandgeneering.com>
2018-02-13 23:00:55 -0800

    add file

---
+++ client/src/myFunctions/normalizeDate.js
@@ -0,0 +1,12 @@
+// to be used to set current date in proper format
+/*const normalizedDate = (date) => {
+		let year = date.getFullYear();
+		let month = date.getMonth()+1;
+		let day = date.getDate();
+		let fullDate =[];
+		// return '2019-12-31'
+		return [...fullDate, year, month, day].join('-');
+	};
+
+let _today = new Date()//.toLocaleDateString()
+console.log(normalizedDate(_today).toString())*/
