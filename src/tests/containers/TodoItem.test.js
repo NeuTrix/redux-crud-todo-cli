e041cfa10be096ca8dd30b4fc03d7576db82aaef
NeuTrix <neutrix@brandgeneering.com>
2018-02-22 19:18:35 -0800

    tweak

--- src/tests/containers/TodoItem.test.js
+++ src/tests/containers/TodoItem.test.js
@@ -1,20 +1,13 @@
 /* eslint-env mocha, chai */
-
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-// ========= connect test component to store
 import { Provider } from 'react-redux';
 import store from '../../store/store';
-
-// Testing...
 import { expect } from 'chai';
-
 import TodoItem from '../../components/TodoItem';
 
-// ====================================
+// ========= 
 
-// this may want a Provider to work
 it('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(
@@ -24,5 +17,3 @@ it('renders without crashing', () => {
 		, div
 	);
 });
-
-// Could not find "store" in either the context or props of "Connect(TodoItem)"
