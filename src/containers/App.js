1ad984641452f952c44b96c9d6139358f2f43883
NeuTrix <neutrix@brandgeneering.com>
2018-02-27 04:15:32 -0800

    Comment out addTodo lines in App js. Remove them later
    Redundant vs the createTodo fns

--- src/containers/App.js
+++ src/containers/App.js
@@ -6,7 +6,7 @@ import TodoList from '../containers/TodoList';
 import TodoForm from '../components/TodoForm';
 import Header from '../components/Header';
 
-import { addTodo } from '../actions/todoActions';
+// import { addTodo } from '../actions/todoActions';
 import { createTodo } from '../actions/createActions';
 import { BrowserRouter as Router } from 'react-router-dom'
 
@@ -39,14 +39,14 @@ class App extends Component {
 // ========= Props ========= 
 
 App.propTypes = { 
-	addTodo:    PropTypes.func.isRequired,
+	// addTodo:    PropTypes.func.isRequired,
 	api:    PropTypes.string.isRequired,
 	createTodo:    PropTypes.func.isRequired,
 	todoArray: 	PropTypes.array.isRequired
 };
 
 App.defaultProps ={
-	addTodo: f=>f,
+	// addTodo: f=>f,
  	api: 'https://redux-todo-api.herokuapp.com/api/todos',
 	createTodo: f=>f,
 	todoArray: []
@@ -62,7 +62,7 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		addTodo: (task) => { dispatch(addTodo(task)) },
+		// addTodo: (task) => { dispatch(addTodo(task)) },
 		createTodo: (api, task) => { dispatch(createTodo(api, task)) },
 		startApp: (url) => { dispatch(startState(url)) }
 	};
