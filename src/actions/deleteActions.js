f49c5d8232c3d517b49e8a68336a5de4a80648c4
NeuTrix <neutrix@brandgeneering.com>
2018-02-28 20:02:24 -0800

    Scrubbed

--- src/actions/deleteActions.js
+++ src/actions/deleteActions.js
@@ -6,55 +6,40 @@ export const DELETE_IS_POSTING  = 'DELETE_IS_POSTING';
 export const DELETE_HAS_SUCCEEDED = 'DELETE_HAS_SUCCEEDED';
 export const DELETE_HAS_ERRORED = 'DELETE_HAS_ERRORED';
 
-export function deleteIsPosting(bool)  {
+export function deleteIsPosting (bool) {
 	return {
 		type: DELETE_IS_POSTING,
-		payload: {
-			status: bool
-		}
+		payload: { status: bool }
 	};
 }
 
 export const deleteHasSucceeded = (todos) => {
 	return {
 		type: DELETE_HAS_SUCCEEDED,
-		payload: {
-			todos: todos
-		}
+		payload: { todos: todos }
 	};
 };
 
 export const deleteHasErrored = (bool) => {
 	return {
-		type: DELETE_HAS_ERRORED ,
-		payload: {
-			status: bool
-		}
+		type: DELETE_HAS_ERRORED,
+		payload: { status: bool }
 	};
 };
 
-// =========   
-
-// here's the Thunk...
-export function deleteTodo(api,_id) {
-
+export function deleteTodo (api, _id) {
 	return (dispatch) => {
-
-		dispatch(deleteIsPosting(true));
-
-		axios.delete(`${api}/${_id}`)
-			.then((response) => {
-				if(response.status !== 200) {
+		dispatch (deleteIsPosting (true));
+		axios.delete (`${api}/${_id}`)
+			.then ((response) => {
+				if (response.status !== 200) {
 					throw Error(response.statusText);
 				}	
-				// console.log(`the id is: ${_id}`)
-				dispatch(removeTodo(_id));
-				dispatch(deleteIsPosting(false));
+				dispatch (removeTodo (_id));
+				dispatch (deleteIsPosting (false));
 			})
-			.then(() => dispatch(deleteHasSucceeded(true)))
-			.catch((err) => {
-					dispatch(deleteHasErrored(true))
-			});
+			.then (() => dispatch (deleteHasSucceeded (true)))
+			.catch ((err) => { dispatch (deleteHasErrored (true)) });
 	};
 }
 
