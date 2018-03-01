494eed19c156d78c523eff8ce6c16ff92227848a
NeuTrix <neutrix@brandgeneering.com>
2018-02-28 21:48:37 -0800

    scrubbed

--- src/tests/components/CheckComplete.test.js
+++ src/tests/components/CheckComplete.test.js
@@ -1,25 +1,16 @@
 /* eslint-env mocha, chai */
-// Testing resource: https://medium.freecodecamp.org/the-right-way-to-test-react-components-548a4736ab22
-
-// For debugging. Use (inside of an 'it' test, before 'expect'):
-// console.log(mountdCheckComplete.debug())
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { expect } from 'chai';
 import { mount } from 'enzyme';
 import { Provider } from 'react-redux';
 import store from '../../store/store';
-
 import CheckComplete from '../../components/CheckComplete';
-
-// +++++++++ 
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
-	ReactDOM.render(
-		<CheckComplete  /> 
-		, div
+	ReactDOM.render (
+		<CheckComplete  />, div
 	);
 });
 
@@ -41,9 +32,7 @@ describe('The CheckComplete Component', () => {
 	let _tasks = checkComp().find('CheckComplete'); // wrapper
 
 	beforeEach (() => {
-		props = {
-			completed: false,
-		}
+		props = { completed: false }
 		mountdCheckComplete = undefined
 	});
 
@@ -51,22 +40,19 @@ describe('The CheckComplete Component', () => {
 		_state.isChecked = false
 	});
 
+		// to inspect test objects
 		//console.log('*** the Component:', mountdCheckComplete.debug())
 	xit('Inspection beforeEach', () => {
-		// to inspect testing objects
 		console.log('********** state beforeEach',  _state)
 		console.log('********** props beforeEach',  _tasks.props())
 	})
 
 	xit('Inspection afterEach', () => {
-		// to inspect testing objects
-		//console.log('** the Component: ', mountdCheckComplete.debug())
 		console.log('********** state afterEach',  _state)
 		console.log('********** props afterEach',  _tasks.props())
 	})
 
 	describe('the wrapping Checkbox component...', () => {
-
 		const checkboxes = checkComp().find('Checkbox');
 		const wrappingRow = checkboxes.first();
 
@@ -77,11 +63,9 @@ describe('The CheckComplete Component', () => {
 		it('always renders a CheckComplete component ', () => {
 			expect(_tasks.length).to.be.eql(1);
 		})
-
 	});
 
 	describe(' Then component PROPS...', () => {
-
 		xit('Inspection for Rendering', () => {
 			// to inspect testing objects
 			console.log('*** Render the State: ', _state) 
@@ -111,11 +95,9 @@ describe('The CheckComplete Component', () => {
 		it('...has an toggleComplete prop', () => {
 			expect(Object.keys(_tasks.props())).to.include('toggleComplete')
 		})
-
 	})
 
 	describe('The component STATE', () => {
-
 		it('...has (1) state items', () => {
 			expect(Object.keys(_state).length).to.eql(1)
 		})
@@ -123,17 +105,14 @@ describe('The CheckComplete Component', () => {
 		it('... has an isChecked state ', () => {
 			expect(_state).to.have.property('isChecked')
 		})
-
 	})
 
 	xdescribe('..after toggline', () => {
-
 		beforeEach(() => {
 			_tasks.props().completed = true
 		});
 			let _tasks1 = checkComp().find('CheckComplete'); // wrapper
 			let _state1 = checkComp().state(); // mounted state 
-
 
 		it('Inspection: Editing', () => {
 			// to inspect testing objects
@@ -153,7 +132,5 @@ describe('The CheckComplete Component', () => {
 		xit('...changes isChecked state to true', () => {
 			expect(_state1.isChecked).to.eql(true)
 		})
-
 	})
-
 })
