940a64c1b911f68d7745b900d56d7d17438623b6
NeuTrix <neutrix@brandgeneering.com>
2018-02-28 21:04:28 -0800

    scrubbed

--- src/tests/reducerTests/editReducer.test.js
+++ src/tests/reducerTests/editReducer.test.js
@@ -1,70 +1,49 @@
 /* eslint-env jest, mocha, chai */
-
-// ======== esllint
 /*eslint no-undef: "error"*/
-
 import axios from 'axios';
-
 import deepFreeze from 'deep-freeze';
 import { expect } from 'chai';
 import store from '../../store/store';
-
-import { 
-	editIsPosting,
-	editHasSucceeded,
-	editHasErrored,
-	editTodo
-} from '../../actions/editActions';
-
 import { startState } from '../../actions/readActions'
-
-// +++++++++ 
+import * as actions from '../../actions/editActions';
 
 describe('The editReducer action suite', () => {
 	let initialState;
-
 	beforeAll(() => {
 		initialState = store.getState().editApi;
 		deepFreeze(initialState);
 	});
 
 	describe('The POSTING reducer', () => {
-
 		afterAll(() => {
-			store.dispatch(editIsPosting(false));
+			store.dispatch(actions.editIsPosting(false));
 		});
 
 		it('... has an isLoading prop set to false', () => {
 			expect(initialState).to.have.property('editIsPosting')
 				.to.eql(false);
 		});
-
 	});
 
 	describe('The SUCCESS reducer', () => {
-
 		afterEach(() => {
-			store.dispatch(editHasSucceeded(false));
+			store.dispatch(actions.editHasSucceeded(false));
 		});
 
 		it('... the initialState has an editHasSucceeded prop set to false', () => {
 			expect(initialState).to.have.property('editHasSucceeded')
 				.to.eql(false);
 		});
-
 	});
 
 	describe('The ERROR reducer', () => {
-
 		afterAll(() => {
-			store.dispatch(editHasErrored(false));
+			store.dispatch(actions.editHasErrored(false));
 		});
 
-		it('... initialState has an editHasErrored prop set to false', () => {
+		it('... initialState has a false editHasErrored prop', () => {
 			expect(initialState).to.have.property('editHasErrored')
 				.to.eql(false);
 		});
-
 	});
-
 });
