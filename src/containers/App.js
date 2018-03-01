ce98c757ab73b374eed0dfc621fe40da036d1afb
NeuTrix <neutrix@brandgeneering.com>
2018-02-28 21:30:50 -0800

    scrubbed

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,12 @@
 import React, { Component } from 'react';
-import { connect } from 'react-redux';
-
 import PropTypes from 'prop-types';
 import TodoList from '../containers/TodoList';
 import TodoForm from '../components/TodoForm';
 import Header from '../components/Header';
-
-// import { addTodo } from '../actions/todoActions';
+import { connect } from 'react-redux';
 import { createTodo } from '../actions/createActions';
 import { BrowserRouter as Router } from 'react-router-dom'
-
 import { startState } from '../actions/readActions'
-
-// ========= 
 
 class App extends Component {
 	
@@ -36,23 +30,17 @@ class App extends Component {
 	}
 } 
 
-// ========= Props ========= 
-
 App.propTypes = { 
-	// addTodo:    PropTypes.func.isRequired,
 	api:    PropTypes.string.isRequired,
 	createTodo:    PropTypes.func.isRequired,
 	todoArray: 	PropTypes.array.isRequired
 };
 
 App.defaultProps ={
-	// addTodo: f => f,
  	api: 'https://redux-todo-api.herokuapp.com/api/todos',
 	createTodo: f => f,
 	todoArray: [ ]
 };
-
-// ========= Store  ========= 
 
 const mapStateToProps = (state) => {
 	return {
@@ -62,7 +50,6 @@ const mapStateToProps = (state) => {
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		// addTodo: (task) => { dispatch(addTodo(task)) },
 		createTodo: (api, task) => { dispatch(createTodo(api, task)) },
 		startApp: (url) => { dispatch(startState(url)) }
 	};
