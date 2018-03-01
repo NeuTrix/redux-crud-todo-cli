e4e56883a23f937fc9d6d4da39757328f29adb5b
NeuTrix <neutrix@brandgeneering.com>
2018-02-28 22:03:48 -0800

    scrubbed

--- src/tests/reducerTests/apiReducer.test.js
+++ src/tests/reducerTests/apiReducer.test.js
@@ -1,22 +1,10 @@
 /* eslint-env jest, mocha, chai */
-
-// ======== esllint
 /*eslint no-undef: "error"*/
-
 import shortid from 'shortid';
-
 import deepFreeze from 'deep-freeze';
 import { expect } from 'chai';
 import store from '../../store/store';
-
-import { 
-	todosIsLoading,
-	todosHasFetched,
-	todosHasErrored,
-	// getTodosData
-} from '../../actions/readActions';
-
-// +++++++++ ===========================
+import * as _a from '../../actions/readActions';
 
 describe('The apiReducer action suite', () => {
 	let initialState;
@@ -27,54 +15,47 @@ describe('The apiReducer action suite', () => {
 	});
 
 	describe('The LOADING reducer', () => {
-
 		afterAll(() => {
-			store.dispatch(todosIsLoading(false));
+			store.dispatch(_a.todosIsLoading(false));
 		});
 
 		it('... initialState has an isLoading prop set to false', () => {
-			expect(initialState).to.have.property('todosIsLoading')
+			expect (initialState).to.have.property('todosIsLoading')
 				.to.eql(false);
 		});
 
 		it('... should immutably change the state to true', () => {
-			store.dispatch(todosIsLoading(true));
+			store.dispatch(_a.todosIsLoading(true));
 			let loading = store.getState().todosApi.todosIsLoading;
-			expect(loading).to.eql(true);
+			expect (loading).to.eql(true);
 		});
-
 	});
 
 	xdescribe('The SUCCESS reducer', () => {
-
 		afterEach(() => {
-			store.dispatch(todosHasFetched(false));
+			store.dispatch(_a.todosHasFetched(false));
 		});
 
-		it('... initialState has an todosHasFetched prop set to false', () => {
-			expect(initialState).to.have.property('todosHasFetched')
+		it('... initialState has an _a.todosHasFetched prop set to false', () => {
+			expect (initialState).to.have.property('_a.todosHasFetched')
 				.to.eql(false);
 		});
-
 	});
 
 	describe('The ERROR reducer', () => {
-
 		afterAll(() => {
-			store.dispatch(todosHasErrored(false));
+			_a.store.dispatch(todosHasErrored(false));
 		});
 
 		it('... initialState has an todosHasErrored prop set to false', () => {
-			expect(initialState).to.have.property('todosHasErrored')
+			expect (initialState).to.have.property('todosHasErrored')
 				.to.eql(false);
 		});
 
 		it('... should immutably change the state to true', () => {
-			store.dispatch(todosHasErrored(true));
+			store.dispatch(_a.todosHasErrored(true));
 			let hasFetched = store.getState().todosApi.todosHasErrored;
-			expect(hasFetched).to.eql(true);
+			expect (hasFetched).to.eql(true);
 		});
-
 	});
-
 });
