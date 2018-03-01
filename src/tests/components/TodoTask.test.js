be435d91cbfeac9f24e39a209fcde88415ca8429
NeuTrix <neutrix@brandgeneering.com>
2018-02-28 21:56:05 -0800

    scrubbed

--- src/tests/components/TodoTask.test.js
+++ src/tests/components/TodoTask.test.js
@@ -1,6 +1,4 @@
 /* eslint-env mocha, chai */
-
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { expect } from 'chai';
@@ -9,24 +7,20 @@ import { Provider } from 'react-redux';
 import store from '../../store/store';
 import TodoTask from '../../components/TodoTask';
 
-it('renders without crashing', () => {
+it ('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(
-		<TodoTask  /> 
-		, div
+		<TodoTask  />, div
 	);
 });
 
-describe('The TodoTask Component', () => {
-	let 
-		props, // default props to clear objects before each test
-	 	mountedTodoTask // the mounted object
+describe ('The TodoTask Component', () => {
+	let props, // default props to clear objects before each test
+	let mountedTodoTask // the mounted object
 
 	const todotaskComp = () => {
-		if(!mountedTodoTask) {
-			mountedTodoTask = mount (
-				<TodoTask {...props}/> 
-			);
+		if (!mountedTodoTask) {
+			mountedTodoTask = mount (<TodoTask {...props}/>);
 		}
 		return mountedTodoTask
 	};
@@ -56,111 +50,98 @@ describe('The TodoTask Component', () => {
 		_state.isEditing = false
 	});
 
-	xit('Inspection beforeEach', () => {
+	xit ('Inspection beforeEach', () => {
 		// to inspect testing objects
 		// console.log('*** the Component: ', mountedTodoTask.debug())
 		console.log('********** state beforeEach',  _state)
 		console.log('********** props beforeEach',  _tasks.props())
 	})
 
-	xit('Inspection afterEach', () => {
-		// to inspect testing objects
-		// console.log('*** the Component: ', mountedTodoTask.debug())
+	xit ('Inspection afterEach', () => {
 		console.log('********** state afterEach',  _state)
 		console.log('********** props afterEach',  _tasks.props())
 	})
 
-	describe('the wrapping Form component...', () => {
-
+	describe ('the wrapping Form component...', () => {
 		const form = todotaskComp().find('Form');
 		const formCtl = todotaskComp().find('FormControl');
 		const wrappingRow = formCtl.first();
 
-		it('always renders an outer FormControl', () => {
-			expect(formCtl.length).to.be.eql(1);
+		it ('always renders an outer FormControl', () => {
+			expect (formCtl.length).to.be.eql(1);
 		})
 
-		it('always renders a TodoTask component ', () => {
-			expect(_tasks.length).to.be.eql(1);
+		it ('always renders a TodoTask component ', () => {
+			expect (_tasks.length).to.be.eql(1);
 		})
 
 	});
 
-	describe(' When rendering, for Props...', () => {
-
-		xit('Inspection for Rendering', () => {
+	describe (' When rendering, for Props...', () => {
+		xit ('Inspection for Rendering', () => {
 			// to inspect testing objects
 			// console.log('*** the Component: ', mountedTodoTask.debug())
 			console.log('*** Render the State: ', _state) 
 			console.log('*** Render the Props: ', _tasks.props()) 
 		})
 
-		it('...TodoTask is passed (4) props', () => {
-			expect(Object.keys(_tasks.props()).length).to.eql(4)
+		it ('...TodoTask is passed (4) props', () => {
+			expect (Object.keys(_tasks.props()).length).to.eql(4)
 		})
 
-		it('...has an "api" prop', () => {
-			expect(Object.keys(_tasks.props())).to.include('api')
+		it ('...has an "api" prop', () => {
+			expect (Object.keys(_tasks.props())).to.include('api')
 		})
 
 		it.only('... api has the correct value', () => {
 			let api = (_tasks.props().api)
-			expect(api).to.eql('https://redux-todo-api.herokuapp.com/api/todos')
+			expect (api).to.eql('https://redux-todo-api.herokuapp.com/api/todos')
 		})
 
-		it('...has an item prop', () => {
-			expect(Object.keys(_tasks.props())).to.include('item')
+		it ('...has an item prop', () => {
+			expect (Object.keys(_tasks.props())).to.include('item')
 		})
 
-		it('...has an style prop', () => {
-			expect(Object.keys(_tasks.props())).to.include('style')
+		it ('...has an style prop', () => {
+			expect (Object.keys(_tasks.props())).to.include('style')
 		})
 
-		it('...has an editTodo prop', () => {
-			expect(Object.keys(_tasks.props())).to.include('editTodo')
+		it ('...has an editTodo prop', () => {
+			expect (Object.keys(_tasks.props())).to.include('editTodo')
 		})
-
-
 	})
 
-	describe('The component STATE', () => {
-
-		it('...has (3) state items', () => {
-			expect(Object.keys(_state).length).to.eql(3)
+	describe ('The component STATE', () => {
+		it ('...has (3) state items', () => {
+			expect (Object.keys(_state).length).to.eql(3)
 		})
 
-		it('...has style ', () => {
-			expect(_state).to.have.property('style')
+		it ('...has style ', () => {
+			expect (_state).to.have.property('style')
 		})
 
-		it('... starts with a false isEditing ', () => {
-			expect(_state).to.have.property('isEditing')
+		it ('... starts with a false isEditing ', () => {
+			expect (_state).to.have.property('isEditing')
 				.to.eql(false)
 		})
 
-		it('...has isCompleted ', () => {
-			expect(_state).to.have.property('isCompleted')
+		it ('...has isCompleted ', () => {
+			expect (_state).to.have.property('isCompleted')
 		})
 		
 	})
 
-	xdescribe('..when editing', () => {
-
+	xdescribe ('..when editing', () => {
 		beforeEach(() => {
 			_state.isCompleted = true ,
 			_state.isEditing = true 
 		});
 
-		xit('Inspection: Editing', () => {
+		xit ('Inspection: Editing', () => {
 			// to inspect testing objects
 			console.log('*** Edit State: ', _state) 
 			console.log('*** Edit Props: ', _tasks.props()) 
 			console.log('*** Edit Props: ', _tasks) 
 		})
-
-		xit('...changes font color to red', () => {
-
-		})
 	})
-
 })
