bdd49c049a17ad67163e69a45c275513a32ed44a
NeuTrix <neutrix@brandgeneering.com>
2018-03-09 15:55:46 -0800

    refactor- remove signup from loginPgae

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -3,7 +3,6 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import LoginForm from './LoginForm';
 import { Col, Grid, Row } from 'react-bootstrap';
-import { userSignupRequest } from '../../actions/signupActions'
 import { userLoginRequest } from '../../actions/loginActions'
 import { addFlashMessage } from '../../actions/flashActions'
 
@@ -18,14 +17,13 @@ class LoginPage extends Component {
     }
 
     // deconstruction
-    const { userSignupRequest, userLoginRequest,addFlashMessage } = this.props;
+    const { userLoginRequest, addFlashMessage } = this.props;
 
     return (
     <Grid >
       <Row>
         <Col md = { 4 } mdoffset = { 4 }  style = { style } >
           <LoginForm 
-            userSignupRequest = { userSignupRequest } 
             userLoginRequest = { userLoginRequest } 
             addFlashMessage = { addFlashMessage } 
           />
@@ -37,15 +35,13 @@ class LoginPage extends Component {
 }
 
 LoginPage.propTypes = {
-  userSignupRequest: PropTypes.func.isRequired,
   userLoginRequest: PropTypes.func.isRequired,
   addFlashMessage: PropTypes.func.isRequired
 }
 
 LoginPage.defaultProps = {
-  userSignupRequest: f => f,
   userLoginRequest: f => f,
   addFlashMessage: f => f
 }
 
-export default connect(null, { userSignupRequest, userLoginRequest, addFlashMessage })(LoginPage);
+export default connect(null, { userLoginRequest, addFlashMessage })(LoginPage);
