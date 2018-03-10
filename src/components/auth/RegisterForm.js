84d4d0c3c22039458d6c72133e90ecd25a144e9a
NeuTrix <neutrix@brandgeneering.com>
2018-03-10 00:51:26 -0800

    Peronsalize success regist message

--- src/components/auth/RegisterForm.js
+++ src/components/auth/RegisterForm.js
@@ -41,10 +41,10 @@ class RegisterForm extends Component {
     if (this.isValid()) {
       this.setState({ errors: { }, isLoading: true }); // reset state
       this.props.userSignupRequest(this.state)
-        .then(() => { 
+        .then((res) => { 
             this.props.addFlashMessage({
               type: 'success',
-              text: ' Welcome! You have successfully Registered.'
+              text: `Welcome ${ res.data.username} ! You have successfully Registered.`
             })
             this.context.router.history.push('/'); 
           }, 
