e53f0b47995fe40b6f230d120e6fda37600eb0b3
NeuTrix <neutrix@brandgeneering.com>
2018-03-11 10:29:36 -0700

    fix create errors

---
+++ src/actions/configApiAddress.js
@@ -0,0 +1,8 @@
+// single source for api address changes
+
+let herokuRoute = 'https://redux-todo-api.herokuapp.com'
+let localRoute = 'http://localhost:3003'
+
+// select an api
+export const baseApi = localRoute
+// export const baseApi = herokuRoute
