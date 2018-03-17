f344c942538239b80f4451203794d7e632c3cfd3
NeuTrix <neutrix@brandgeneering.com>
2018-03-17 01:17:19 -0700

    Clear default todo from reducer

--- src/reducers/todoReducer.js
+++ src/reducers/todoReducer.js
@@ -9,9 +9,7 @@ import {
 	UPDATE_TASK
 } from '../actions/todoActions';
 
-let defaultState =[ {	task: 'Welcome! Start by entering new Todo\'s in the box above.' } ];
-
-const TodoReducer = (state = defaultState , action) => {
+const TodoReducer = (state = [ ] , action) => {
 	let payload  = action.payload;
 	let type = action.type;
 
