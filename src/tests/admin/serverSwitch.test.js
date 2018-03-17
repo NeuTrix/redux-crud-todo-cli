713436765b450abf54b16e77d766c6724423d875
NeuTrix <neutrix@brandgeneering.com>
2018-03-16 21:33:06 -0700

    Add template for server switch test

---
+++ src/tests/admin/serverSwitch.test.js
@@ -0,0 +1,60 @@
+import axios from 'axios'
+import chai, { expect } from 'chai'
+import { createTodo } from '../../actions/createActions';
+import { deleteTodo } from '../../actions/deleteActions';
+import { editTodo } from '../../actions/editActions';
+import { userLoginRequest } from '../../actions/loginActions';
+import { userSignupRequest } from '../../actions/signupActions';
+import { startState } from '../../actions/readActions';
+import { 
+	getHeroku, 
+	getLocal 
+} from '../../actions/admin/serverSwitchActions.js'
+
+
+describe ('The serverSwitchActions', () => {
+
+	let apiLocal = 'http://localhost:3003/api/todos' 
+	let apiHeroku = 'https://redux-todo-api.herokuapp.com/api/todos'
+
+
+	describe ('... can connect to the heroku base url', () => {
+
+
+		it ('... sets the createTodo api to heroku', () => {
+
+			expect()
+		});
+
+		it ('... sets the startState api to heroku', () => {
+
+		});
+
+		it ('... sets the editTodo api to heroku', () => {
+
+		});
+
+		it ('... sets the deleteTodo api to heroku', () => {
+
+		});
+
+		it ('... sets the userLoginRequest api to heroku', () => {
+
+		});
+
+		it ('... sets the userSignupRequest api to heroku', () => {
+
+		});
+
+
+it ('... sets the createTodo api to heroku', () => {
+
+		});
+
+	});
+
+	describe ('... can swith to the local host url', () => {
+
+	});
+
+});
