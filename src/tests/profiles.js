e80fe5ce09a5e725c244a27f491bf7d0a687a9aa
NeuTrix <neutrix@brandgeneering.com>
2018-03-18 12:09:15 -0700

    add test stub file for login

---
+++ src/tests/profiles.js
@@ -0,0 +1,109 @@
+
+export const user_01 = {
+	username: HenryPimm,
+	email: hp@pimmindustries.com,
+	emailConfirm: hp@pimmindustries.com,
+	password: AntMan,
+	passwordConfirm: AntMan,
+}
+
+export const user_02 = {
+	username:LukeCage,
+	email:Bpm@harlem.com,
+	emailConfirm:Bpm@harlem.com,
+	password:powerMan,
+	passwordConfirm:powerMan,
+}
+
+export const user_03 = {
+	username:Wonder,
+	email:ww@ww.com,
+	emailConfirm:ww@ww.com,
+	password:Woman,
+	passwordConfirm:Woman,
+}
+
+export const findTask ={
+ "completed": false,
+  "date": "1936-05-24T00:00:00.000Z",
+  "details": "Default Details",
+  "rank": "High",
+  "task": "Welcome LukeCage! Start entering new Todo's in the box above.",
+  "_id": "5aacc7d2ad73050020b50eb7",
+  "owner": "5aab5ca424a17be27ea2f12d",
+  "__v": 0
+}
+
+export const todoArray = [
+
+    {
+        "completed": false,
+        "date": "1936-05-24T00:00:00.000Z",
+        "details": "Default Details",
+        "rank": "High",
+        "task": "Welcome LukeCage! Start entering new Todo's in the box above.",
+        "_id": "5aacc7d2ad73050020b50eb7",
+        "owner": "5aab5ca424a17be27ea2f12d",
+        "__v": 0
+    },
+    {
+        "completed": false,
+        "date": "2018-03-17T00:00:00.000Z",
+        "details": "Default Details",
+        "rank": "Med",
+        "task": "asdfa",
+        "_id": "5aacc7eaad73050020b50eb9",
+        "owner": "5aab5ca424a17be27ea2f12d",
+        "__v": 0
+    },
+    {
+        "completed": false,
+        "date": "1936-05-24T00:00:00.000Z",
+        "details": "Default Details",
+        "rank": "High",
+        "task": "Welcome LukeCage! Start entering new Todo's in the box above.",
+        "_id": "5aacc837ad73050020b50eba",
+        "owner": "5aab5ca424a17be27ea2f12d",
+        "__v": 0
+    },
+    {
+        "completed": false,
+        "date": "2018-03-17T00:00:00.000Z",
+        "details": "Default Details",
+        "rank": "Med",
+        "task": "Added a component",
+        "_id": "5aacc841ad73050020b50ebb",
+        "owner": "5aab5ca424a17be27ea2f12d",
+        "__v": 0
+    },
+    {
+        "completed": false,
+        "date": "1936-05-24T00:00:00.000Z",
+        "details": "Default Details",
+        "rank": "High",
+        "task": "Welcome LukeCage! Start entering new Todo's in the box above.",
+        "_id": "5aacc846ad73050020b50ebc",
+        "owner": "5aab5ca424a17be27ea2f12d",
+        "__v": 0
+    },
+    {
+        "completed": false,
+        "date": "2018-03-17T00:00:00.000Z",
+        "details": "Default Details",
+        "rank": "Med",
+        "task": "why isnt my shit saving?",
+        "_id": "5aacc85cad73050020b50ebd",
+        "owner": "5aab5ca424a17be27ea2f12d",
+        "__v": 0
+    },
+    {
+        "completed": false,
+        "date": "1936-05-24T00:00:00.000Z",
+        "details": "Default Details",
+        "rank": "High",
+        "task": "Welcome LukeCage! Start entering new Todo's in the box above.",
+        "_id": "5aacc865ad73050020b50ebe",
+        "owner": "5aab5ca424a17be27ea2f12d",
+        "__v": 0
+    }
+] 
