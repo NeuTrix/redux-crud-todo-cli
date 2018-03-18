f2d9c5c4abdb9a62603640938161d90ee27002d5
NeuTrix <neutrix@brandgeneering.com>
2018-03-18 12:10:57 -0700

    tweaks

---
+++ src/tests/auth/loginAndRegister.test.js
@@ -0,0 +1,53 @@
+/* eslint-env node, mocha, chai, jest */
+import { expect } from 'chai';
+import axios from 'axios';
+
+describe('The API request object actions', () => {
+	// let url = 'https://redux-todo-api.herokuapp.com'
+	let url = 'https://localhost:3003'
+	let user = // array
+	let _task; // initial test item
+	let _last; // last test item
+
+	beforeAll((done) => {
+		axios.get('https://redux-todo-api.herokuapp.com/api/todos')
+			.then((res) => {
+				todos = res.data;
+				_last = todos.length - 1;
+				_task = todos[0];
+				done();
+			});
+	});
+
+	describe('the retrieved todo items ', () => {
+		it('... are an array of objects', () => {
+			expect(todos.length).to.be.above(0);
+			expect(todos).to.be.an('array');
+			expect(_task).to.be.an('object');
+		});
+
+		it('... have a "completed" property', () => {
+			expect(_task).to.have.property('completed');
+		});
+				
+		it('... have a "date" property', () => {
+			expect(_task).to.have.property('date');
+		});
+				
+		it('... have a "details" property', () => {
+			expect(_task).to.have.property('details');
+		});
+				
+		it('... have a "owner" property', () => {
+			expect(_task).to.have.property('owner');
+		});
+				
+		it('... have a "rank" property', () => {
+			expect(_task).to.have.property('rank');
+		});
+				
+		it('... have a "task" property', () => {
+			expect(_task).to.have.property('task');
+		});
+	});
+})
