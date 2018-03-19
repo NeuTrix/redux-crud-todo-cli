79ad803335d71833d98e034558d2286940b93456
NeuTrix <neutrix@brandgeneering.com>
2018-03-18 19:23:35 -0700

    User is authenticated at registration

--- src/actions/signupActions.js
+++ src/actions/signupActions.js
@@ -1,4 +1,16 @@
 import axios from 'axios';
+import setAuthorizationToken from '../components/auth/setAuthToken'
+import jwtDecode from 'jwt-decode'
+import { SET_CURRENT_USER } from './typeConstants'
+
+export function setCurrentUser (user) {
+	return {
+		type: SET_CURRENT_USER,
+		payload: {
+			user
+		}
+	}
+};
 
 export function userSignupRequest (userData) {
 
@@ -6,7 +18,19 @@ export function userSignupRequest (userData) {
 	// let url = 'http://localhost:3003'
 	
 	return dispatch => {
-		return axios.post(`${ url }/api/auth/register`, userData);
+		return axios.post(`${ url }/api/auth/register`, userData)
+		.then((res) => {
+
+			const token = res.data.token;
+			localStorage.setItem('jwtToken', token);
+
+			setAuthorizationToken(token);
+			const user = jwtDecode(token)
+			dispatch(setCurrentUser(user))
+
+			return res
+
+		})
 	}
 }
 
