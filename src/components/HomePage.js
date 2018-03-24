17cdd0af81f6e7ad7e13a6bf6e7649606079e60e
NeuTrix <neutrix@brandgeneering.com>
2018-03-24 11:00:21 -0700

    tweaks

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, ButtonToolbar, Col, Grid, Jumbotron, Row } from 'react-bootstrap'
+import { Button, ButtonToolbar, Col, Grid, Row } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const style ={
