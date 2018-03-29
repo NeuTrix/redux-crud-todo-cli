4619f9cedaf40db3249a103a165b564d9dd425bc
NeuTrix <neutrix@brandgeneering.com>
2018-03-28 20:35:01 -0700

    refactor for mods

--- src/reducers/apiReducer.js
+++ src/reducers/apiReducer.js
@@ -1,9 +1,4 @@
-// import action constants- allows JS to throw spelling errors
-import {
-	TODOS_IS_LOADING,
-	TODOS_HAS_FETCHED,
-	TODOS_HAS_ERRORED,
-} from '../actions/typeConstants';
+import * as mod from '../actions/typeConstants';
 
 export const apiReducer = (
 	
@@ -19,14 +14,23 @@ export const apiReducer = (
 
 	switch(action.type) {
 
-	case TODOS_IS_LOADING:
-		return {...state, ...{todosIsLoading:payload.status}};
+	case mod.TODOS_IS_LOADING:
+		return {
+			...state, 
+			...{ todosIsLoading:payload.status }
+		};
 	
-	case TODOS_HAS_FETCHED:
-		return {...state, ...{todosHasFetched:payload.status}};
+	case mod.TODOS_HAS_FETCHED:
+		return {
+			...state, 
+			...{ todosHasFetched:payload.status }
+		};
 	
-	case TODOS_HAS_ERRORED:
-		return {...state, ...{todosHasErrored:payload.status}};
+	case mod.TODOS_HAS_ERRORED:
+		return {
+			...state, 
+			...{ todosHasErrored:payload.status }
+		};
 
 	default:
 		return state;
