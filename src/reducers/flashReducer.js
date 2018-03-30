475a7f3f6d632ae6084d3065d209a5ce41ce1708
NeuTrix <neutrix@brandgeneering.com>
2018-03-29 23:13:08 -0700

    refact flash delete msg

---
+++ src/reducers/flashReducer.js
@@ -0,0 +1,29 @@
+import * as mod from '../actions/typeConstants';
+import shortid from 'shortid';
+import findIndex from 'lodash/findIndex';
+
+export const flashReducer = (state = [], action = {}) => {
+
+	let _pay = action.payload;
+
+	switch (action.type) {
+
+	case mod.ADD_FLASH_MESSAGE:
+		return [
+			...state, 
+			{ 
+				_id: shortid.generate(),
+				type:	_pay.message.type,
+				text: _pay.message.text 
+			}
+		];
+
+	case mod.DELETE_FLASH_MESSAGE:
+		return state.filter( msg => msg._id !== _pay._id)
+
+	default:
+		return state;
+	}
+};
+	
+export default flashReducer;
