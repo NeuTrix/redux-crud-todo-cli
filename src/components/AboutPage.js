069e6dcbe82c638e55dc8459c1405e4032873428
NeuTrix <neutrix@brandgeneering.com>
2018-04-02 18:05:01 -0700

    starting About page with LinkedIn - not working

---
+++ src/components/AboutPage.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const AboutPage = (props) => {
+
+	return(
+
+		<div>
+
+			<div 
+				className= "LI-profile-badge"  
+				data-version = "v1" 
+				data-size = "medium" 
+				data-local = "en_US" 
+				data-type = "vertical" 
+				data-theme = "light" 
+				data-vanity = "walkermj"
+			>
+				<a 
+					className="LI-simple-link" 
+					href='https://www.linkedin.com/in/walkermj?trk=profile-badge'
+				> Michael Jay Walker </a>
+			</div>
+		
+		</div>
+		)
+};
+
+export default AboutPage
