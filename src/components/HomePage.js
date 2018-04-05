1afe6fefdc6106d1e0f1bf1473dcf77a73a37820
NeuTrix <neutrix@brandgeneering.com>
2018-04-05 13:15:02 -0700

    Final update of Home page layout

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Button, ButtonToolbar, Col, Grid, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-const style ={
+const style = {
 	background: 'aliceblue',
 	border: ' 2px solid navy',
 	borderRadius: 5,
@@ -13,18 +13,16 @@ const style ={
 };
 
 const styleBtn = {
-	// marginTop: 15,
-	marginLeft: 1,
-	// marginRight: 1,
 	color: 'whitesmoke',
 	width: 70
 };
 
+const styleList = { fontSize: '1em', textAlign: 'left' }
+
 const space = { 
-	xs: { head: 12, list: 9, btns1: 3, btns2: 3 }, 
+	xs: { head: 12, list: 8, btns1: 3, btns2: 3 }, 
 	sm: { head: 12, list: 4, btns1: 3, btns2: 3 }, 
 	md: { head: 12, list: 4, btns1: 3, btns2: 3 }, 
-	lg: { head: 12, list: 4, btns1: 3, btns2: 3 }, 
 };
 
 // +++++++++   +++++++++ 
@@ -36,7 +34,7 @@ const HomePage = (props) => {
 		  	<Col style = { style } >
 
 		  		<Row>
-		  			<Col xs = { space.xs.head } >
+		  			<Col xs = { space.xs.head } style = {{ textAlign: 'center'}}  >
 						  <h2>MERN Todo </h2>
 						  <p> Fullstack CRUD application featuring: </p>
 		  			</Col>
@@ -44,49 +42,50 @@ const HomePage = (props) => {
 
 		  		<Row>
 		  			<Col 
-		  				xs = { space.xs.list } xsOffset = { 3 } 
-		  				sm = { space.sm.list } smOffset = { 4 } 
-		  				md = { space.md.list } mdOffset = { 4 } 
-							lg = { space.lg.list } lgOffset = { 4 } 
+		  				xs = { space.xs.list } xsOffset = { 2 } 
+		  				sm = { space.sm.list } smOffset = { 5 } 
+		  				md = { space.md.list } mdOffset = { 5 } 
 	  				>
 							<ul>
-						  	<li style = { { textAlign: 'left', fontSize: '1em'}} >
-							  	Mongo | Mongoose
-						  	</li>
-						  	<li style = { { textAlign: 'left', fontSize: '1em'}} >
-							  	Express
-						  	</li>
-						  	<li style = { { textAlign: 'left', fontSize: '1em'}} >
-							  	React | Redux
-						  	</li>
-						  	<li style = { { textAlign: 'left', fontSize: '1em'}} >
-							  	Node
-						  	</li>
+						  	<li style = { styleList } > Mongo | Mongoose </li>
+						  	<li style = { styleList } > Express </li>
+						  	<li style = { styleList } > React | Redux </li>
+						  	<li style = { styleList } > Node </li>
 							</ul>
 		  			</Col>
 		  		</Row>
 
 		  		<Row>
 		  			<Col 
-		  				style =  {{ textAlign: 'right', outline: '1px solid orange'}}
-							xs = { space.xs.btns1 } xsOffset = {3}
-							
-	  				>
+	  					className =  'signinBtn'
+		  				style = {{ textAlign: 'right' }}
+			  			xs = { space.xs.btns1 } xsOffset = {3} 
+			  			sm = { space.sm.btns1 } smOffset = {3} 
+		  			>
 	  					<Link to = '/login' >
-			  				<Button  bsSize = 'small'style = { styleBtn } bsStyle = 'success'> 
+			  				<Button  
+				  				bsSize = 'small' 
+				  				style = { styleBtn } 
+				  				bsStyle = 'success'
+			  				> 
 				  				Sign in 
 		  					</Button>
 	  					</Link >
 		  			</Col>
 			  			
 	  				<Col 
-		  				style =  {{textAlign: 'left', outline: '1px solid orange'}}
-							xs = { space.xs.btns2 }
-						
+	  					className =  'registerBtn'
+		  				style = {{ textAlign: 'left' }}
+		  				xs = { space.xs.btns2 } 
+		  				sm = { space.sm.btns2 } 
 	  				>
 	  					<Link to = '/register' >
-			  				<Button style = { styleBtn } bsSize = 'small'   bsStyle = 'primary'> 
-			  				Register
+			  				<Button 
+				  				style = { styleBtn } 
+				  				bsSize = 'small'   
+				  				bsStyle = 'primary'
+			  				> 
+				  				Register
 		  					</Button>
 	  					</Link >
 		  			</Col>
