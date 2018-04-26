87863f5737d0d35dd2b686fd37f89c122c65f6b7
NeuTrix <neutrix@brandgeneering.com>
2018-04-25 17:19:38 -0700

    Initial homepage layout grid

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,98 +1,93 @@
 import React from 'react';
-import { Button, ButtonToolbar, Col, Grid, Row } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 const style = {
+grid : {
+	// grid layout
+	display: 'grid',
+	gridTemplateAreas: 
+	` "title title title title"
+	  "subt subt subt subt"
+	  "apps apps apps apps"
+	  ".. login regis .."   `,
+
+  //  base styling
 	background: 'aliceblue',
 	color: 'navy',
 	marginRight: 5,
 	opacity: 0.7,
 	padding: 8,
-	textAlign: 'center',
+	textAlign: 'center'
+	},
+
+	title: { 
+		gridArea: 'title',
+	},
+
+	subt: { 
+		gridArea: 'subt',
+	},
+
+	apps: { 
+		gridArea: 'apps',
+	},
+
+	login: { 
+		gridArea: 'login',
+	},
+
+	regis: { 
+		gridArea: 'regis',
+	},
+
+
 };
 
-const styleBtn = {
-	color: 'whitesmoke',
-	width: 70
-};
 
-const styleList = { fontSize: '1em', textAlign: 'left' };
-
-const space = { 
-	xs: { head: 12, list: 8, btns1: 3, btns2: 3 }, 
-	sm: { head: 12, list: 4, btns1: 3, btns2: 3 }, 
-	md: { head: 12, list: 4, btns1: 3, btns2: 3 }, 
-};
-
-// +++++++++   +++++++++ 
+// +++++++++ COMPONENT  +++++++++ 
 
 const HomePage = (props) => {
 	return (
-		<Grid>
-			<Row>
-		  	<Col style = { style } >
+		<div style = { style.grid } >
+			
+			<div style = {{ gridArea: 'title'}} > 
+				<h1>Li'l Todo app (beta)</h1> 
+			</div>  
 
-		  		<Row>
-		  			<Col xs = { space.xs.head } style = {{ textAlign: 'center'}}  >
-						  <h2>MERN Todo </h2>
-						  <p> Fullstack CRUD application featuring: </p>
-		  			</Col>
-		  		</Row>
+			<div style = { style.subt } > 
+				<h2>MERN stack application</h2> 
+			</div>  
 
-		  		<Row>
-		  			<Col 
-		  				xs = { space.xs.list } xsOffset = { 2 } 
-		  				sm = { space.sm.list } smOffset = { 5 } 
-		  				md = { space.md.list } mdOffset = { 5 } 
-	  				>
-							<ul>
-						  	<li style = { styleList } > Mongo | Mongoose </li>
-						  	<li style = { styleList } > Express </li>
-						  	<li style = { styleList } > React | Redux </li>
-						  	<li style = { styleList } > Node </li>
-							</ul>
-		  			</Col>
-		  		</Row>
+			<div style = { style.apps } >  
+				<h3>Mongo | Mongoose</h3>
+			</div> 
 
-		  		<Row>
-		  			<Col 
-	  					className =  'signinBtn'
-		  				style = {{ textAlign: 'right' }}
-			  			xs = { space.xs.btns1 } xsOffset = {3} 
-			  			sm = { space.sm.btns1 } smOffset = {3} 
-		  			>
-	  					<Link to = '/login' >
-			  				<Button  
-				  				bsSize = 'small' 
-				  				style = { styleBtn } 
-				  				bsStyle = 'success'
-			  				> 
-				  				Sign in 
-		  					</Button>
-	  					</Link >
-		  			</Col>
-			  			
-	  				<Col 
-	  					className =  'registerBtn'
-		  				style = {{ textAlign: 'left' }}
-		  				xs = { space.xs.btns2 } 
-		  				sm = { space.sm.btns2 } 
-	  				>
-	  					<Link to = '/register' >
-			  				<Button 
-				  				style = { styleBtn } 
-				  				bsSize = 'small'   
-				  				bsStyle = 'primary'
-			  				> 
-				  				Register
-		  					</Button>
-	  					</Link >
-		  			</Col>
-		  		</Row>
+			<div style = { style.apps } >  
+				<h3>Express</h3>
+				
+			</div> 
 
-				</Col>
-			</Row>
-		</Grid>
+			<div style = { style.apps } >  
+				<h3>React | Redux</h3>
+				
+			</div> 
+
+			<div style = { style.apps } >  
+				<h3>NodeJS </h3>
+				
+			</div>  
+
+			<div style = { style.login } >  
+				<button>
+				</button>
+			</div>  
+
+			<div style = { style.signin } >  
+				<button>
+				</button>
+			</div>  
+
+		</div>
 	);
 };
 
