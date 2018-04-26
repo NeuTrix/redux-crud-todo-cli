15fd6d1f90b4075869926f8814dbfd98ab41529e
NeuTrix <neutrix@brandgeneering.com>
2018-04-26 16:59:09 -0700

    Refactored Login button and fixed gridArea display issue

---
+++ src/components/LoginButton.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
+const style = { 
+	backgroundColor: 'lightgreen', 
+	borderColor: 'green',
+	color: 'green',
+}
+
+const LoginButton = ({ area }) => {
+	return (
+		<Link to = '/login' style = {{ gridArea: area }} >
+			<button className= 'button material' style= { style } >
+				Login
+			</button>  
+		</Link >
+		)
+}
+
+LoginButton.propTypes = {
+	area: PropTypes.string.isRequired
+}
+
+export default LoginButton
