924dae605442486f037991e367bba395bfe963e2
NeuTrix <neutrix@brandgeneering.com>
2018-04-26 15:07:47 -0700

    Update homepage styles and App.css classes

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,134 +2,102 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const style = {
-grid : {
-	// grid layout
-	display: 'grid',
-	gridTemplateAreas: 
-	` "title title title title"
-	  "subt subt subt subt"
-	  "apps apps apps apps" 
-	  "apps apps apps apps" 
-	  ".. login regis .."   `,
-	// gridAutoFlow: 'row',
-	gridTemplateColumns: 'repeat(4, 1fr)',
-	gridGap: '25px 5px',
-	justifyItems: 'center',
-	alignItems: 'center',
+// grid layout
+	grid : {
+		display: 'grid',
+		gridTemplateAreas: 
+		` "title title title title"
+		  "subt subt subt subt"
+		  "apps apps apps apps" 
+		  "apps apps apps apps" 
+		  ".. login regis .."   `,
+		gridTemplateColumns: 'repeat(4, 1fr)',
+		gridGap: '25px 5px',
 
-  //  base styling
-	background: 'white',
-	borderRadius: 5,
-	border: '2px solid navy',
-	marginRight: 5,
-	opacity: 0.9,
-	padding: 8,
-	textAlign: 'center'
+	  //  base styling
+		background: 'white',
+		padding: 15,
 	},
 
-	title: { 
-		gridArea: 'title',
-		backgroundColor: 'whitesmoke',
-		borderRadius: 5,
-		width: '100%',
-	},
+	//callouts
 
-	subt: { 
-		gridArea: 'subt',
-		marginBottom: 20,
-	},
+	title: { gridArea: 'title' },
+	subt: { gridArea: 'subt',border: 'none' },
+	apps: { color: 'orange', borderColor: 'orange' },
+	feats: { color: 'navy', borderColor: 'navy' },
 
-	apps: { 
-		display: 'flex',
-		color: 'orange',
-		border: '3px solid orange',
-		// backgroundColor: '#ffdd77',
-		backgroundColor: 'whitesmoke',
-		borderRadius: 5,
-		minHeight: 80,
-		width: 80,
-		justifyContent: 'center',
-		alignItems: 'center',
-	},
+	// buttons
 
 	login: { 
-		gridArea: 'login',
-		backgroundColor: '#ccffcc',
-		border: '2px solid green',
+		backgroundColor: 'lightgreen', 
+		borderColor: 'green',
 		color: 'green',
-		height: 30,
-		width: 80,
-		borderRadius: 5,
 	},
 
 	regis: { 
-		gridArea: 'regis',
 		backgroundColor: 'lightblue',
-		border: '2px solid navy',
-		opacity: '100%',
+		borderColor: 'navy',
 		color: 'navy',
-		height: 30,
-		width: 80,
-		borderRadius: 5,
 	},
-
-
 };
-
 
 // +++++++++ COMPONENT  +++++++++ 
 
 const HomePage = (props) => {
 	return (
-		<div style = { style.grid } >
+		<div className = 'box' style = { style.grid } >
 			
-			<div style = {{ gridArea: 'title'}, style.title} > 
+			<div className = 'box' style = {{ gridArea: 'title'}, style.title} > 
 				<h3>Li'l Todo (beta)</h3> 
 			</div>  
 
-			<div style = { style.subt } > 
+			<div className = 'box' style = { style.subt } > 
 				<h5>... a little Fullstack MERN CRUD web app featuring:</h5> 
 			</div>  
 
-			<div style = {{ gridColumn: '1 / 2' }, style.apps } >  
+			<div className = 'box' className = 'callout box' style = {{ gridColumn: '1 / 2' }, style.apps } >  
 				<h4>Mongo</h4>
 			</div> 
 
-			<div style = {{ gridColumn: '2 / 3' }, style.apps } >  
+			<div className = 'callout box' style = {{ gridColumn: '2 / 3' }, style.apps } >  
 				<h4>Express</h4>
 			</div> 
 
-			<div style = {{ gridColumn: '4 / 5' }, style.apps } >  
+			<div className = 'callout box' style = {{ gridColumn: '4 / 5' }, style.apps } >  
 				<h4>React</h4>
 			</div> 
 
-			<div style = {{ gridColumn: '3 / 4' }, style.apps } >  
+			<div className = 'callout box' style = {{ gridColumn: '3 / 4' }, style.apps } >  
 				<h4>NodeJS</h4>
 			</div> 
 
-			<div style = {{ gridColumn: '1 / 2' }, style.apps } >  
-				<h5>Redux</h5>
-			</div> 
-
-			<div style = {{ gridColumn: '2 / 3' }, style.apps } >  
-				<h5>Login Auth</h5>
-			</div> 
-
-			<div style = {{ gridColumn: '4 / 5' }, style.apps } >  
-				<h5>CSS Grid</h5>
-			</div> 
-
-			<div style = {{ gridColumn: '3 / 4' }, style.apps } >  
+			<div className = 'callout box' style = {{ gridColumn: '1 / 2' }, style.feats } >  
 				<h5>Decoupled</h5>
 			</div> 
 
-			<button style = { style.login } >  
-					Login
-			</button>  
+			<div className = 'callout box' style = {{ gridColumn: '2 / 3' }, style.feats } >  
+				<h5>CSS Grid</h5>
+			</div> 
 
-			<button style = { style.regis } >  
-				Register
-			</button>  
+			<div className = 'callout box' style = {{ gridColumn: '4 / 5' }, style.feats } >  
+				<h5>Redux</h5>
+			</div> 
+
+			<div className = 'callout box' style = {{ gridColumn: '3 / 4' }, style.feats } >  
+				<h5>Login/Auth</h5>
+			</div> 
+
+			<Link to = '/login' style = {{ gridArea: 'login'}} >
+				<button className = 'button' style = { style.login } >  
+					Login
+				</button>  
+			</Link >
+
+			<Link to = '/register' style = {{ gridArea: 'regis'}} >
+				<button className = 'button' style = { style.regis } >  
+					Register
+				</button>  
+			</Link>
 
 		</div>
 	);
