98f212596961553b33112e292b729db963809b8e
NeuTrix <neutrix@brandgeneering.com>
2018-04-26 19:12:45 -0700

    Clean up files

--- src/index.js
+++ src/index.js
@@ -1,17 +1,16 @@
+import registerServiceWorker from './registerServiceWorker';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider }  from 'react-redux';
+import jwtDecode from 'jwt-decode';
+import store from './store/store';
 import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
+
 import App from './containers/App';
 import setAuthorizationToken from './components/auth/setAuthToken';
-import jwtDecode from 'jwt-decode';
 import { setCurrentUser } from './actions/loginActions';
-
-import { Provider }  from 'react-redux';
-import store from './store/store';
-
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 let root = document.getElementById('root');
 let token = localStorage.jwtToken;
