55717fd1be50e11cbfacd62411d87a4477155c2d
NeuTrix <neutrix@brandgeneering.com>
2018-04-29 16:30:43 -0700

    Set up initial container page for TodoPage and basic test file'

---
+++ src/components/todos/TodoPage.js
@@ -0,0 +1,32 @@
+// vendor
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+import { connect } from 'redux';
+
+// custom
+
+// +++++++++  CSS  +++++++++ 
+
+class TodoPage extends Component {
+
+	render() {
+
+		return (
+			<div>
+				TodoPage
+			</div>
+			)
+	}
+
+}
+
+
+// +++++++++  COMPONENT  +++++++++ 
+
+
+// +++++++++  PROPS  +++++++++ 
+
+
+
+export default (TodoPage)
