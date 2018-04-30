aead3ee9ecb692908d1af4111452674d6e863aa8
NeuTrix <neutrix@brandgeneering.com>
2018-04-30 12:07:24 -0700

    tweaks

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -5,18 +5,13 @@ import { userLoginRequest } from '../../actions/loginActions';
 import { addFlashMessage } from '../../actions/flashActions';
 import { connect } from 'react-redux';
 
-const style = {
-	padding: 20,
-}
-
 const LoginPage = (props) => {
 	
 	return (
-
 		<div 
 			id = 'loginPage' 
 			className = 'boxClr engrBox' 
-			style= { style }  
+			style= {{ padding: 20 }}  
 		>
 			<LoginForm   
 				userLoginRequest = { props.userLoginRequest } 
--- src/components/auth/RegisterForm.js
+++ src/components/auth/RegisterForm.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+// custom
 import validateInput from '../../helpers/signupValidator';
 import TextFieldGroup from './TextFieldGroup';
 import BasicButton from '../buttons/BasicButton';
