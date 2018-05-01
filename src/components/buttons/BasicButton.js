861f591e26f4803baad5249b7befaea6c89ebad8
NeuTrix <neutrix@brandgeneering.com>
2018-05-01 08:07:00 -0700

    Tweak code BasicButton for styling and inherit className

--- src/components/buttons/BasicButton.js
+++ src/components/buttons/BasicButton.js
@@ -1,29 +1,33 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 
-const BasicButton = ({ bgColor, color, name }) => {
+const BasicButton = ({ className, bgColor, color, name }) => {
 
-	const style = {
-		color,
-		borderColor: color,
-		backgroundColor: bgColor,
-	}
+	const Button = styled.button `
+		${ color },
+		borderColor: ${ color },
+		backgroundColor: ${ bgColor },
+	`;
 
 	return (
-		<button className= 'btn mat opac' style= { style } >
+		<Button className= { `BasicButton ${ className } btn mat` } >
 			{ name }
-		</button>  
+		</Button>  
 	)
 }
 
 BasicButton.propTypes = {
-	color: PropTypes.string, // indicates link color
 	bgColor: PropTypes.string, // indicates link color
+	color: PropTypes.string, // indicates link color
+	className: PropTypes.string.isRequired, // for styled-component
 	name: PropTypes.string.isRequired, // altenative name for
 }
 
 BasicButton.defaultProps = {
-	name: 'Name Me!'
+	className: 'BasicButton',
+	color: 'lightgrey',
+	name: 'Name Me!',
 }
 
 export default BasicButton
