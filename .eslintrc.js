12f1fac238e04cc931db78164c5a40b0887c74a6
NeuTrix <neutrix@brandgeneering.com>
2018-09-15 05:09:56 -0700

    Fix eslint.
    Needed to include in .nodemodule/bin...

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,21 @@
 module.exports = {
-    "extends": "airbnb"
+    "extends": [
+        "google",
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "env": { 
+        "es6": true,
+        "mocha": true
+     },
+    "parserOptions": {
+        "ecmaVersion": 6,
+            "sourceType": "module",
+                "ecmaFeatures": {
+            "jsx": true
+        }
+    },
+    "rules": {
+        "semi": 2
+}
 };
--- src/components/App.js
+++ src/components/App.js
@@ -2,13 +2,13 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import PropTypes from 'prop-types';
-import { connect } from 'react-redux';
-import { Route } from 'react-router-dom';
+import {connect} from 'react-redux';
+import {Route} from 'react-router-dom';
 import styled from 'styled-components';
 // custom
 import './material.css';
 import requireAuth from '../helpers/requireAuth';
-import { logout } from '../actions/loginActions';
+import {logout} from '../actions/loginActions';
 import FlashMessageList from './messages/FlashMessageList';
 import NavBar from './main/NavBar';
 import Home from './main/Home';
@@ -17,18 +17,18 @@ import RegisterPage from './auth/RegisterPage';
 import TodoPage from './todos/TodoPage';
 // CSS
 const Grid = styled.div `
-		display: grid;
-		grid-template-areas:   
-			" header " 
-			" messages "  
-			" main "  
-		;
-		grid-template-columns: 1fr;
-		grid-gap: 10px;
-		grid-template-rows: auto;
-		font-family: arial;
-		padding: 0px 10px 0px 10px;
-	`;
+  display: grid;
+  grid-template-areas:   
+    " header " 
+    " messages "  
+    " main "  
+  ;
+  grid-template-columns: 1fr;
+  grid-gap: 10px;
+  grid-template-rows: auto;
+  font-family: arial;
+  padding: 0px 10px 0px 10px;
+`;
 
 const Header = styled.div `
 		grid-area: header;
@@ -39,50 +39,52 @@ const Main = styled.div `grid-area: main;`;
 
 // Component
 const App = (props) => {
-	return (
-		<Grid className= 'App' >
+  return (
+    <Grid className= 'App' >
 
-			<Header className= 'Header' > 
-				<NavBar 
-					authApi= { props.authApi } 
-					logout= { props.logout }
-				/> 
-			</Header>
+      <Header className= 'Header' >
+        <NavBar
+          authApi= { props.authApi }
+          logout= { props.logout }
+        />
+      </Header>
 
-			<Messages className= 'Messages'>
-				<FlashMessageList/>
-			</Messages>
-			
-			<Main className= 'Main'>
-				<Route exact path= '/' render= {() => 
-					<Home authorized= {props.authApi.isAuthenticated} />} 
-				/>
-				<Route path= '/login' component= { LoginPage } />
-				<Route path= '/register' component= { RegisterPage } />
-				<Route 
-					exact path= '/todos' 
-					component= { requireAuth (ReactDom.render= (props) => 
-						<TodoPage className= 'TodoPage' /> 
-					)}
-				/>
-			</Main>
-		</Grid>
-	);
-}; 
+      <Messages className= 'Messages'>
+        <FlashMessageList/>
+      </Messages>
 
-App.propTypes = { 
-	authApi: 	PropTypes.object.isRequired,
-	logout: 	PropTypes.func.isRequired,
+      <Main className= 'Main'>
+        <Route exact path= '/' render= {() =>
+          <Home authorized= {props.authApi.isAuthenticated} />}
+        />
+        <Route path= '/login' component= { LoginPage } />
+        <Route path= '/register' component= { RegisterPage } />
+        <Route
+          exact path= '/todos'
+          component= { requireAuth(ReactDom.render= (props) =>
+            <TodoPage className= 'TodoPage' />
+          )}
+        />
+      </Main>
+    </Grid>
+  );
+};
+
+App.propTypes = {
+  authApi: PropTypes.object.isRequired,
+  logout: PropTypes.func.isRequired,
 };
 const mapDispatchToProps = (dispatch) => {
-	return { 
-		logout: () => { dispatch (logout ()); }, 
-	};
+  return {
+    logout: () => {
+      dispatch(logout());
+    },
+  };
 };
 const mapStateToProps = (state) => {
-	return {
-		authApi: 	state.authApi,
-	};
-}; 
+  return {
+    authApi: state.authApi,
+  };
+};
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
