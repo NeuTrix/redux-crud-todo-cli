74009c04f2fc3f13dfc73541f8cb7039080da54c
NeuTrix <neutrix@brandgeneering.com>
2018-10-04 23:39:09 -0700

    establish jest test for Home

--- src/test/components/Home.test.js
+++ src/test/components/Home.test.js
@@ -1,3 +1,19 @@
-xit ('... faketest', () => {
-		
+
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { render } from 'enzyme';
+import Home from '../../components/main/Home';
+
+describe('The Home component', () => {
+  const wrapper = render(
+    <Router>
+      <Home/>
+    </Router>
+  );
+
+  it('...renders correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+  
+
 });
