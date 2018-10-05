9c8546303d5c55f7f1e6a003dd39c9b5364c7da2
NeuTrix <neutrix@brandgeneering.com>
2018-10-04 23:38:53 -0700

    Boolean String conv error in component resolved
    The Link in Nav section used booleans to determine visibility
    
    Need to convert these values to strings to pass into styled components
    for DOM rendering
    
    Cleared the warnings

--- src/components/main/NavBar.js
+++ src/components/main/NavBar.js
@@ -1,66 +1,62 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import styled from 'styled-components';
-import logo from '../../assets/done.png';
 import { colors } from '../../helpers/cssConstants';
+import logo from '../../assets/done.png';
+import styled from 'styled-components';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import NavSection from './NavSection';
 
 // +++++++++  CSS  +++++++++ 
 const Grid = styled.div `
 	grid-area: nav-bar;
-
 	grid-template-areas: " logo dash nav-section " ;
 	grid-template-columns: repeat(3, 1fr);
-	display: inline-grid;
-  height: 49px;
-	width: 100%;
-	padding: 0px 0px 10px 0px;
 	border-bottom: 1px solid grey;
 	color: ${colors._olive};
+	display: inline-grid;
+  height: 49px;
+	padding: 0px 0px 10px 0px;
+	width: 100%;
 `;
+// nav logo
 const Logo = styled.img `
 	grid-area: logo;
 	max-width: 65px;
 	place-self: center left;
 	`;
-	const Dash = styled(FontAwesomeIcon) `
+// dashboard icon for pulling up stats
+const Dash = styled(FontAwesomeIcon) `
 	grid-area: dash;
-	font-size: 2em;
+	font-size: 1.5em;
 	place-self: center;
 `;
 // +++++++++  COMPONENT  +++++++++ 
 const NavBar = (props, context) => {
-	const { authApi, logout } = props;
+	const { auth, logout } = props;
 
 	return (
-		<Grid className= 'nav-bar'>
-			<Logo className='logo' src={ logo } alt="logo"/> 
+		<Grid className='nav-bar'>
+			<Logo className='logo' src={ logo } alt='logo'/> 
 			<Dash className='dash-icon' icon='chart-line'/>
 			<NavSection 
-				auth={ authApi.isAuthenticated } 
+				auth={ auth } 
 				logout={ logout } 
-				router='router'
 			/>
 		</Grid>
 	);
 };
 // +++++++++ PROPS  +++++++++ 
 NavBar.propTypes = {
-	authApi: PropTypes.object.isRequired,
+	auth: PropTypes.bool,
 	logout: PropTypes.func.isRequired,
 };
 
 NavBar.defaultProps = {
-	authApi: {
-		user: { username: 'test' }
-	},
-	logout: (f) => alert('Default action: Navbar logout fn'),
+	auth: false,
+	logout: (f) => 'Default action: Navbar logout fn',
 };
 
-NavBar.contextTypes = {
-	router: PropTypes.object.isRequired,
-};
+NavBar.contextTypes = { router: PropTypes.object.isRequired };
 
 export default NavBar;
--- src/components/main/NavSection.js
+++ src/components/main/NavSection.js
@@ -1,85 +1,51 @@
 // Navigation section of the nav bar with toggling menu based on media
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
-// ---------
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import styled from 'styled-components';
 import { colors, media } from '../../helpers/cssConstants';
+import styled from 'styled-components';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { Link } from 'react-router-dom';
 // +++++++++  CSS  +++++++++ 
 const Navigation = styled.div `
 	grid-area: nav-section;
 	grid-template-areas: " menu ";
 	display: inline-grid;
-
-	& * {
-		:hover { color: lime }
+	& * { 
+		color: ${colors._olive};	
+		:hover { color: lime } 
 	}
-	
-	// @media (${media._medium}) {
-	// 	display: inline-grid;
-	// 	// grid-template-areas: " menu "
-	// }
 `;
 // mobbile collapsed menu icon
 const Burger = styled(FontAwesomeIcon) `
 	grid-area: menu;
-
-	color: ${colors._olive};	
 	font-size: 2em;
 	place-self: center right;
-
-	@media (${media._medium}) {
-		display: none;
-	}
+	@media (${media._medium}) { display: none }
 `;
 // full menu for larger screens
 const Menu = styled.ul `
 	grid-area: menu;
 	display: none;
-	
 	@media (${media._medium}) {
 		display: inline-flex;
 		justify-content: space-evenly;
-		// padding: 10px;
 		text-decoration: none;
 	}
 `;
-
-// +++++++++  Elements  +++++++++ 
-const AuthLi = styled.li `
-	display: inline-grid;
-	font-size: initial; //?
-	place-content: center;
-	text-shadow: none;
+// auth list elements
+const AuthLi = styled(Link) `
+	place-content: center right;
 	width: 100px;
-
-	${ ({auth}) => auth 
-		? `display: inline-grid`
-		: `display: none`
-	}
+	${ ({auth}) => auth ? `display: inline-grid`: `display: none` }
 `;
-
-const NoAuthLi = styled.li `
+// unauthorized menu
+const NoAuthLi = styled(Link) `
+	place-content: center right;
 	width: 100px;
-	display: inline-grid;
-	place-content: center;
-	text-shadow: none;
-	font-size: initial
-
-	${({ auth }) => !auth 
-		? `display: inline-grid;` 
-		: `display: none;`
-	}
+	${({ auth }) => !auth ? `display: inline-grid;` : `display: none` }
 `;
-
-const A = styled(Link) `
-	&:hover { color: lime; }
-`;
-
 // +++++++++  COMPONENT  +++++++++ 
-
 const NavSection = (props, context) => {
 	const { auth, logout } = props;
 
@@ -90,53 +56,42 @@ const NavSection = (props, context) => {
 	};
 
 	return (
-
-			<Navigation className='nav-section' > 
+			<Navigation className='nav-section'> 
 
 				<Burger id='burger' icon='bars'/>
 
-				<Menu id='menu' > 
-					<AuthLi auth={ auth } > 
-						<A to='/todos' > Todos </A>
+				<Menu id='menu'> 
+					<AuthLi auth={ auth.toString() }to='/todos'> 
+						Todos 
 					</AuthLi>
 
-					<AuthLi auth={ auth } > 
-						<A to='/#' onClick={ onLogout }> Logout </A>
+					<AuthLi auth={ auth.toString() } to='/#' onClick={ onLogout }> 
+						Logout 
 					</AuthLi>
 					
-					<NoAuthLi auth={ auth } > 
-						<A to='/register' > Register </A>
+					<NoAuthLi auth={ auth.toString() } to='/register'> 
+						Register 
 					</NoAuthLi>
 
-					<NoAuthLi auth={ auth } > 
-						<A to='/login' > Login </A>
+					<NoAuthLi auth={ auth.toString() } to='/login'> 
+						Login 
 					</NoAuthLi>
-
 				</Menu>
 
 			</Navigation>
-
 	);
 };
-
 // +++++++++ PROPS  +++++++++ 
-
 NavSection.propTypes = {
-	authApi: PropTypes.object.isRequired,
+	auth: PropTypes.bool,
 	logout: PropTypes.func.isRequired,
 };
 
 NavSection.defaultProps = {
-	authApi:  {
-		user: {
-			username: 'test'
-		}
-	},
-	logout:  f => alert('Default action: Navbar logout fn'),
+	auth: false,
+	logout: (f) => 'Default action: Navbar logout fn',
 };
 
-NavSection.contextTypes= {
-	router: PropTypes.object.isRequired,
-};
+NavSection.contextTypes = { router: PropTypes.object.isRequired };
 
 export default NavSection;
