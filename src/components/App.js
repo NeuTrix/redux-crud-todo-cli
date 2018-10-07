a8cad76cf35c2a8802ae5853160e1c829effb8e0
NeuTrix <neutrix@brandgeneering.com>
2018-10-07 14:52:44 -0700

    apply mui theme

--- src/components/App.js
+++ src/components/App.js
@@ -1,45 +1,32 @@
 // core react
 import React from 'react';
 import ReactDom from 'react-dom';
-import PropTypes from 'prop-types';
+import { Route } from 'react-router-dom';
 import {connect} from 'react-redux';
-// fontawesome imports and other styles
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import {
-  faBars,
-  faChartLine,
-  faChartBar,
-  faCheckSquare,
-  faCog,
-  faEllipsisH,
-  faTasks,
-  faTachometerAlt,
-} from '@fortawesome/free-solid-svg-icons';
-// import './material.css';
-import { logout } from '../actions/loginActions';
-import requireAuth from '../helpers/requireAuth';
-import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
 // components
-import CssBaseline from '@material-ui/core/CssBaseline';
 import FlashMessageList from './messages/FlashMessageList';
 import Home from './main/Home';
 import LoginPage from './auth/LoginPage';
+import { logout } from '../actions/loginActions';
 import NavBar from './main/NavBar';
 import RegisterPage from './auth/RegisterPage';
-import { Route } from 'react-router-dom';
+import requireAuth from '../helpers/requireAuth';
+import styled from 'styled-components';
 import TodoPage from './todos/TodoPage';
+// import './material.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import lightBlue from '@material-ui/core/colors/lightBlue';
+import lightGreen from '@material-ui/core/colors/lightGreen';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+// fontawesome imports and other styles
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+import { faBars, faChartLine, faChartBar, faCheckSquare, faCog, faEllipsisH, faTasks, faTachometerAlt,
+} from '@fortawesome/free-solid-svg-icons';
 // add to fontawesome library for the app scope
-library.add(
-  fab,
-  faBars,
-  faChartLine,
-  faChartBar,
-  faCog,
-  faCheckSquare,
-  faEllipsisH,
-  faTasks,
-  faTachometerAlt,
+library.add( fab, faBars, faChartLine, faChartBar, faCog, faCheckSquare, faEllipsisH, faTasks, faTachometerAlt,
 );
 // ===> CSS
 const Grid =styled.div `
@@ -55,39 +42,48 @@ const Grid =styled.div `
   font-family: arial;
   padding: 0px 10px 0px 10px;
 `;
-const Messages =styled.div `grid-area: messages;`;
+const Messages = styled.div `grid-area: messages;`;
 const Main =styled.div `grid-area: main;`;
+
+const options = {
+  palette: {
+      primary: lightBlue,
+      secondary: lightGreen,
+    },
+  status: {
+    danger: 'orange',
+  },
+}
+
+const theme = createMuiTheme(options);
 // Component
 const App =(props) => {
   const { authApi, logout} = props;
   const auth = authApi.isAuthenticated;
+
   return (
-    <Grid className='App' >
+    <MuiThemeProvider theme={ theme } >
+      <Grid className='App' >
         <CssBaseline/>
-        <NavBar
-          auth={ auth }
-          logout={ props.logout }
-        />
-
-      <Messages className='Messages'>
-        <FlashMessageList/>
-      </Messages>
-
-      <Main className='Main'>
-        <Route exact path='/' render={ () =>
-          <Home authorized={ auth } />}
-        />
-        <Route path='/login' component={ LoginPage } />
-        <Route path='/register' component={ RegisterPage } />
-        <Route
-          exact path='/todos'
-          component={ requireAuth(ReactDom.render = (props) =>
-            <TodoPage className='TodoPage' />
-          )}
-        />
-      </Main>
-
-    </Grid>
+        <NavBar auth={ auth } logout={ props.logout } />
+        <Messages className='Messages'>
+          <FlashMessageList/>
+        </Messages>
+        <Main className='Main'>
+          <Route exact path='/' render={ () =>
+            <Home authorized={ auth } />}
+          />
+          <Route path='/login' component={ LoginPage } />
+          <Route path='/register' component={ RegisterPage } />
+          <Route
+            exact path='/todos'
+            component={ requireAuth(ReactDom.render = (props) =>
+              <TodoPage className='TodoPage' />
+            ) }
+          />
+        </Main>
+      </Grid>
+    </MuiThemeProvider>
   );
 };
 
