842b42b2e1a99b4ee0fcd26f8857e7615d3d3b7a
NeuTrix <neutrix@brandgeneering.com>
2018-10-07 17:39:03 -0700

    tweak

--- src/components/auth/LoginPage.js
+++ src/components/auth/LoginPage.js
@@ -2,44 +2,42 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+// ===== import components
 import LoginForm from './LoginForm';
-
+// ===== import functions
 import { addFlashMessage } from '../../actions/flashActions';
 import styled from 'styled-components';
 import { userLoginRequest } from '../../actions/loginActions';
-
-const Grid = styled.div`
+// ===== CSS 
+const Grid = styled.div `
 	grid-template-areas: " form ";
-	// grid-template-columns: 1fr;
-
-	border: darkgreen;
 	display: grid;
 	justify-items: center;
 	padding: 20px;
 `;
-
-const LoginPage = (props) => {
+// ===== Main Component 
+const LoginPage = props => {
+	const { addFlashMessage, authApi, userLoginRequest } = this.props;
 	return (
-		<Grid className='LoginPage'>
+		<Grid className='loginPage'>
 			<LoginForm
 				style={{ gridArea: 'form' }}  
-				userLoginRequest={ props.userLoginRequest } 
-				addFlashMessage={ props.addFlashMessage } 
-				authApi={ props.authApi }
+				userLoginRequest={ userLoginRequest } 
+				addFlashMessage={ addFlashMessage } 
+				authApi={ authApi }
 			/>
 		</Grid>
 	);
 };
-
-const mapStateToProps = (state) => {
-	return { 
-		authApi: state.authApi 
-	};
+// ===== Props
+const mapStateToProps = state => {
+	return { authApi: state.authApi };
 };
 
 LoginPage.propTypes = {
 	addFlashMessage: PropTypes.func.isRequired,
-	className: PropTypes.string, // from styled-components
+	// from styled-components...
+	className: PropTypes.string, 
 	userLoginRequest: PropTypes.func.isRequired,
 };
 
