f13116273830ff4b643062ce5ec9762bf41dff76
NeuTrix <neutrix@brandgeneering.com>
2018-10-26 11:59:00 -0700

    Reset navbar layout with alignment
    Begin setting adjustments for nav bar usting a revised simplified grid

--- src/components/main/NavBar.js
+++ src/components/main/NavBar.js
@@ -1,30 +1,33 @@
 // This component holds the full navigation bar 
-
 import React from 'react';
 import PropTypes from 'prop-types';
 import { colors } from '../../helpers/cssConstants';
-import logo from '../../assets/logo-white.png';
 import styled from 'styled-components';
-
-// === components ===
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import NavSection from './NavSection';
+// === components ===
+import logo from '../../assets/logo-white.png';
+// import NavSection from './NavSection';
 
 // ===  CSS  === 
 const Grid = styled.div `
 	grid-area: navBar;
-	grid-template-areas: " logo nav-section ";
-	grid-template-columns: repeat(3, 1fr);
+	grid-template-areas: " menu logo search login ";
+	// grid-template-columns: repeat(4, 1fr);
 	
 	background: steelblue;
-	border-bottom: 1px solid lightgrey ;
+	// border-bottom: 1px solid lightgrey ;
 	display: grid;
-	padding: 5px 0px 5px 0px;
-	width: 100%;
+	// padding: 5px 0px 5px 0px;
+	// width: 100%;
 	& * {
-		color: #fafafa;
+		// color: #fafafa;
+		color: orange;
 	}
 `;
+const Menu = styled.div `
+	grid-area: menu;
+`
+
 // navbar logo
 const Logo = styled.img `
 	grid-area: logo;
@@ -46,9 +49,11 @@ const NavBar = (props, context) => {
 
 	return (
 		<Grid className='nav-bar'>
+			<Menu className='menu'/>
 			<Logo className='logo' src={ logo } alt='logo'/> 
-			<Dash className='dash-icon' icon='tachometer-alt'/>
-			<NavSection auth={ auth } logout={ logout }/>
+			{/* <Dash className='dash-icon' icon='tachometer-alt'/> */}
+			{/* <NavSection auth={ auth } logout={ logout }/> */}
+
 		</Grid>
 	);
 };
--- src/components/main/xNavBar.js
+++ src/components/main/xNavBar.js
@@ -1,216 +1,69 @@
+// This component holds the full navigation bar 
+
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { colors } from '../../helpers/cssConstants';
+import logo from '../../assets/logo-white.png';
 import styled from 'styled-components';
-import logo from '../../assets/done.png';
-// custom
-import { colors, media } from '../../helpers/cssConstants';
 
-// +++++++++  CSS  +++++++++ 
+// === components ===
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import NavSection from './NavSection';
 
-const baseColor = colors._deepblue;
-
+// ===  CSS  === 
 const Grid = styled.div `
+	grid-area: navBar;
+	grid-template-areas: " logo nav-section ";
+	grid-template-columns: repeat(3, 1fr);
+	
+	background: steelblue;
+	border-bottom: 1px solid lightgrey ;
 	display: grid;
-	
-	grid-template-areas: " Home  Home  Navigation " ;
-	grid-template-columns: 1fr 3fr 1fr;
-
-  height: 49px;
+	padding: 5px 0px 5px 0px;
 	width: 100%;
-  // margin-bottom: 40px;
-	padding 5px;
-	
-	color: ${colors._olive};
-
-	// @media (${media._medium}) {
-	// 	grid-template-columns: 2fr 1fr 1fr;
-	// }
-	
+	& * {
+		color: #fafafa;
+	}
 `;
-// +++++++++ Home Section  +++++++++ 
-
-const Home = styled.div `
-	grid-area: Home;z
-	display: inline-grid;
-	place-content: center;
-	grid-template-areas: "Logo Welcome";
-	grid-template-columns: 1fr 3fr;
-`;
-
+// navbar logo
 const Logo = styled.img `
-	grid-area: Logo;
-	// text-indent: 10px;
-	// text-decoration: none;
-	&:hover { color:lime; }
-	width: 50px;
+	grid-area: logo;
+	max-width: 75px;
+	place-self: center left;
 `;
 
-const Message = styled.div `
-	grid-area: Welcome;
-	place-content: center;
-	display: inline-grid;
-`;
-// +++++++++ Nav Section  +++++++++ 
-
-const Navigation = styled.div `
-	grid-area: Navigation;
-	display: inline-grid;
-	place-content: center;
-	padding-right: 5px;
-
-	&:hover { color:lime; }
-
-	@media (max-width: 630px) {
-		 &:hover ul {
-			display: inline-grid;
-			grid-row-gap: 15px
-			position: absolute;
-			width: 100%;
-			min-height: 59px;
-			right: 0px;
-			top: 54px;
-			background: ${baseColor};
-			opacity: .75;
-		}
-	}
-	
-	@media (${media._medium}) {
-		display: inline-grid;
-		grid-template-areas: "Menu"
-	}
+// dashboard icon for pulling up stats
+const Dash = styled(FontAwesomeIcon) `
+	// color: ${colors._iceblue};
+	grid-area: dash;
+	font-size: 2em;
+	place-self: center;
 `;
 
-const Burger = styled.div `
-	display: inline-grid;
-	place-content: center right;
-	color: ${colors._iceblue};	
-	&:hover { color: lime;}
-
-	@media (${media._medium}) {
-		display: none;
-	}
-`;
-
-const Menu = styled.ul `
-	grid-area: Menu;
-	display: none;
-	place-content: center;
-
-	padding: 10px;
-	margin: 0;
-	text-decoration: none;
-
-	@media (${media._medium}) {
-		display: inline-flex;
-		justify-content: space-evenly;
-	}
-`;
-
-// +++++++++  Elements  +++++++++ 
-const AuthLi = styled.li `
-	width: 100px;
-
-	display: inline-grid;
-	place-content: center;
-	text-shadow: none;
-	font-size: initial
-
-	${ ({auth}) => auth ? `
-		display: inline-grid;` : `
-		display: none;
-	`}
-`;
-
-
-
-const NoAuthLi = styled.li `
-	width: 100px;
-	display: inline-grid;
-	place-content: center;
-	text-shadow: none;
-	font-size: initial
-
-	${({ auth }) => !auth 
-		? `display: inline-grid;` 
-		: `display: none;`
-	}
-`;
-
-const A = styled(Link) `
-	&:hover { color: lime; }
-`;
-
-// +++++++++  COMPONENT  +++++++++ 
-
+// ===  Main Component  === 
 const NavBar = (props, context) => {
-
-	const onLogout = (e) => {
-		e.preventDefault();
-		props.logout();
-		context.router.history.push('/login');
-	};
-
-	const auth = props.authApi.isAuthenticated;
-	const name = props.authApi.user.username;
+	const { auth, logout } = props;
 
 	return (
-
-		<Grid className= 'NavBar' >
-
-			<Home id= 'Home'>
-
-				<Logo className='logo' src={logo} alt="logo"/> 
-
-				<Message id= 'Message' > 
-					{ auth ? `Welcome ${ name }!` : 'Please Log in!' }
-				</Message>
-
-			</Home>
-				
-			<Navigation id= 'Navigation' > 
-
-				<Burger id= 'Burger' className= ' engr fa fa-navicon fa-2x'/>
-
-				<Menu id= 'Menu' > 
-					<AuthLi auth= { auth } > 
-						<A to= '/todos' > Todos </A>
-					</AuthLi>
-
-					<AuthLi auth= { auth } > 
-						<A to= '/#' onClick= { onLogout }> Logout </A>
-					</AuthLi>
-					
-					<NoAuthLi auth= { auth } > 
-						<A to= '/register' > Register </A>
-					</NoAuthLi>
-
-					<NoAuthLi auth= { auth } > 
-						<A to= '/login' > Login </A>
-					</NoAuthLi>
-
-				</Menu>
-
-			</Navigation>
-
+		<Grid className='nav-bar'>
+			<Logo className='logo' src={ logo } alt='logo'/> 
+			<Dash className='dash-icon' icon='tachometer-alt'/>
+			<NavSection auth={ auth } logout={ logout }/>
 		</Grid>
 	);
 };
 
-// +++++++++ PROPS  +++++++++ 
-
+// === Props  === 
 NavBar.propTypes = {
-	authApi: PropTypes.object.isRequired,
+	auth: PropTypes.bool,
 	logout: PropTypes.func.isRequired,
 };
 
 NavBar.defaultProps = {
-	authApi:  {},
-	logout:  f => alert('Default action: Navbar logout fn'),
+	auth: false,
+	logout: (f) => 'Default action: Navbar logout fn',
 };
 
-NavBar.contextTypes= {
-	router: PropTypes.object.isRequired,
-};
+// NavBar.contextTypes = { router: PropTypes.object.isRequired };
 
 export default NavBar;
