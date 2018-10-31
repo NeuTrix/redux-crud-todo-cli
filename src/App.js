561ffed5f31d29f374c18791e62b566859f2a673
NeuTrix <neutrix@brandgeneering.com>
2018-10-31 12:35:52 -0700

    Refactored App.js for linting and styleguide

--- src/App.js
+++ src/App.js
@@ -1,38 +1,20 @@
-// core react
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import React from 'react';
 import ReactDom from 'react-dom';
 import { Route } from 'react-router-dom';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 
 import CssBaseline from '@material-ui/core/CssBaseline';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fab } from '@fortawesome/free-brands-svg-icons';
-import {
-	faBars,
-	faChartLine,
-	faChartBar,
-	faCheckSquare,
-	faCog,
-	faEllipsisH,
-	faTasks,
-	faTachometerAlt,
-} from '@fortawesome/free-solid-svg-icons';
 import { withStyles } from '@material-ui/core/styles';
 
 import FlashMessageList from './components/messages/FlashMessageList';
 import Home from './components/main/Home';
 import LoginPage from './components/auth/LoginPage';
+import { logout } from './actions/loginActions';
 import MenuDrawer from './components/main/MenuDrawer';
 import RegisterPage from './components/auth/RegisterPage';
-import TodoPage from './components/todos/TodoPage';
-import { logout } from './actions/loginActions';
 import requireAuth from './helpers/requireAuth';
-
-// add to fontawesome library for the app scope
-library.add(fab, faBars, faChartLine, faChartBar,
-	faCog, faCheckSquare, faEllipsisH, faTasks, faTachometerAlt);
-
+import TodoPage from './components/todos/TodoPage';
 
 const propTypes = {
 	authApi: PropTypes.instanceOf(Object).isRequired,
@@ -50,17 +32,17 @@ const mapStateToProps = state => ({
 
 function App(props) {
 	const { authApi, handleLogout, classes } = props;
-	const auth = authApi.isAuthenticated; // boolean val to pass down to children
+	const isAuth = authApi.isAuthenticated; // boolean val to pass down to children
 
 	return (
 		<div className={`grid ${classes.grid}`}>
 			<CssBaseline />
-			<MenuDrawer auth={auth} logout={handleLogout} />
+			<MenuDrawer auth={isAuth} logout={handleLogout} />
 			<div className={`messages ${classes.messages}`}>
 				<FlashMessageList />
 			</div>
 			<div className={`main ${classes.main}`}>
-				<Route exact path="/" render={() => <Home authorized={auth} />} />
+				<Route exact path="/" render={() => <Home authorized={isAuth} />} />
 				<Route path="/login" component={LoginPage} />
 				<Route path="/register" component={RegisterPage} />
 				<Route
@@ -87,15 +69,11 @@ const styles = () => ({
 		marginTop: '50px',
 		padding: '0px 10px 0px 10px',
 	},
-	main: {
-		gridArea: 'main',
-	},
-	messages: {
-		gridArea: 'messages',
-	},
+	main: { gridArea: 'main' },
+	messages: { gridArea: 'messages' },
 });
 
 App.propTypes = propTypes;
 App.displayName = 'App';
 
-export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles, { withTheme: true })(App));
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles)(App));
