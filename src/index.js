7c02ea8a04dc712f5a269ca7a9c6ee8280ad5ed0
NeuTrix <neutrix@brandgeneering.com>
2018-10-31 10:19:30 -0700

    update Theme provider defaults

--- src/index.js
+++ src/index.js
@@ -1,62 +1,64 @@
 import 'font-awesome/css/font-awesome.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider }  from 'react-redux';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import jwtDecode from 'jwt-decode';
+import styled from 'styled-components';
 import registerServiceWorker from './registerServiceWorker';
 import store from './store/store';
-import styled from 'styled-components';
 
 import setAuthorizationToken from './components/auth/setAuthToken';
 import { setCurrentUser } from './actions/registerActions';
 import App from './App';
 // import material-ui
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
-		
+
 
 import green from '@material-ui/core/colors/green';
+// // import white from '@material-ui/core/colors/white';
 import lightBlue from '@material-ui/core/colors/lightBlue';
-   
 
 
-let root = document.getElementById('root');
-let token = localStorage.jwtToken;
+
+const root = document.getElementById('root');
+const token = localStorage.jwtToken;
 
 if (token) {
 	setAuthorizationToken(token);
 	store.dispatch(setCurrentUser(jwtDecode(token)));
-} 
+}
 
-const Store = styled(Provider) `
+const Store = styled(Provider)`
   margin: 0px;
   padding: 0px;
   font-family: sans-serif;
 `;
 
 const options = {
-  palette: {
-      primary: lightBlue,
-      secondary: green,
-    },
-  status: {
-    danger: 'orange',
-  },
-   typography: {
-     useNextVariants: true,
-   },
-}
+	palette: {
+    primary: lightBlue,
+    secondary: green,
+    contrast: 'whitesmoke'
+	},
+	status: {
+		danger: 'orange',
+	},
+	typography: {
+		useNextVariants: true,
+	},
+};
 
 const theme = createMuiTheme(options);
 
 ReactDOM.render(
-	<Store store={ store } >
-	 <MuiThemeProvider theme={ theme } >
-			<Router>
-				<Route path='/' component={ App } />
-			</Router>
-    </MuiThemeProvider>
-	</Store>, root
+	<Store store={store}>
+		<Router>
+      <MuiThemeProvider theme={theme}>
+        <Route path="/" component={App} />
+      </MuiThemeProvider>
+    </Router>
+	</Store>, root,
 );
 
 registerServiceWorker();
