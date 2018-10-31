f0296acebc9672ffca596140b061aa7dfc0ad453
NeuTrix <neutrix@brandgeneering.com>
2018-10-30 20:17:33 -0700

    build

---
+++ build/precache-manifest.be1b878af38d4da7ff09740f144c0d56.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "21938a91ea66094d26f0",
+    "url": "/NeuTrix/redux_crud_todolist/static/js/main.21938a91.chunk.js"
+  },
+  {
+    "revision": "ffc886fe712a9b32ad49",
+    "url": "/NeuTrix/redux_crud_todolist/static/css/1.cf53e139.chunk.css"
+  },
+  {
+    "revision": "ffc886fe712a9b32ad49",
+    "url": "/NeuTrix/redux_crud_todolist/static/js/1.ffc886fe.chunk.js"
+  },
+  {
+    "revision": "2ae438f05d59c1a0f14a",
+    "url": "/NeuTrix/redux_crud_todolist/static/js/runtime~main.2ae438f0.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/NeuTrix/redux_crud_todolist/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/NeuTrix/redux_crud_todolist/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/NeuTrix/redux_crud_todolist/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/NeuTrix/redux_crud_todolist/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/NeuTrix/redux_crud_todolist/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "a6f252cde26766a21445419cdb4e261f",
+    "url": "/NeuTrix/redux_crud_todolist/index.html"
+  }
+];
