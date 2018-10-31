73c65d2eeabf7f200d8a690c134294b139066ba4
NeuTrix <neutrix@brandgeneering.com>
2018-10-31 11:57:48 -0700

    Update style and props code for TodoPage
    - Grid classname and classes def used in wrapper div via str templates
    
    - Dumped 'styled-components'
    
    - Move Props definitions to top of code
    
    - Move styles to bottom of code and inject into component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import { Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
-import styled from 'styled-components';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fab } from '@fortawesome/free-brands-svg-icons';
@@ -19,73 +18,25 @@ import {
 	faTasks,
 	faTachometerAlt,
 } from '@fortawesome/free-solid-svg-icons';
+import { withStyles } from '@material-ui/core/styles';
+
 import FlashMessageList from './components/messages/FlashMessageList';
 import Home from './components/main/Home';
 import LoginPage from './components/auth/LoginPage';
 import MenuDrawer from './components/main/MenuDrawer';
-// import Navigation from './main/Navigation';
-// import NavTest from './components/main/NavTest';
 import RegisterPage from './components/auth/RegisterPage';
 import TodoPage from './components/todos/TodoPage';
-// functions
 import { logout } from './actions/loginActions';
 import requireAuth from './helpers/requireAuth';
-
-
-// fontawesome imports and other styles
 
 // add to fontawesome library for the app scope
 library.add(fab, faBars, faChartLine, faChartBar,
 	faCog, faCheckSquare, faEllipsisH, faTasks, faTachometerAlt);
 
-// ===> CSS
-const Grid = styled.div`
-  grid-template-areas:   
-    " navBar " 
-    " messages "  
-    " main "  
-  ;
-  grid-template-rows: auto;
-  grid-gap: 10px;
 
-  display: grid;
-  font-family: arial;
-  margin-top: 50px;
-  padding: 0px 10px 0px 10px;
-`;
-const Messages = styled.div`grid-area: messages;`;
-const Main = styled.div`grid-area: main;`;
-// material-ui custom styling
-
-// ==== main Component
-function App(props) {
-	const { authApi, handleLogout } = props;
-	const auth = authApi.isAuthenticated; // boolean val to pass down to children
-
-	return (
-		<Grid className="App">
-			<CssBaseline />
-			<MenuDrawer auth={auth} logout={handleLogout} />
-			<Messages className="Messages">
-				<FlashMessageList />
-			</Messages>
-			<Main className="Main">
-				<Route exact path="/" render={() => <Home authorized={auth} />} />
-				<Route path="/login" component={LoginPage} />
-				<Route path="/register" component={RegisterPage} />
-				<Route
-					exact
-					path="/todos"
-					component={requireAuth(ReactDom.render = () => (<TodoPage className="TodoPage" />))}
-					// component={requireAuth(ReactDom.render = props => (<TodoPage className="TodoPage" />))}
-				/>
-			</Main>
-		</Grid>
-	);
-}
-// ===== Props
-App.propTypes = {
+const propTypes = {
 	authApi: PropTypes.instanceOf(Object).isRequired,
+	classes: PropTypes.instanceOf(Object).isRequired,
 	handleLogout: PropTypes.func.isRequired,
 };
 
@@ -96,5 +47,55 @@ const mapDispatchToProps = dispatch => ({
 const mapStateToProps = state => ({
 	authApi: state.authApi,
 });
-// =====
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+
+function App(props) {
+	const { authApi, handleLogout, classes } = props;
+	const auth = authApi.isAuthenticated; // boolean val to pass down to children
+
+	return (
+		<div className={`grid ${classes.grid}`}>
+			<CssBaseline />
+			<MenuDrawer auth={auth} logout={handleLogout} />
+			<div className={`messages ${classes.messages}`}>
+				<FlashMessageList />
+			</div>
+			<div className={`main ${classes.main}`}>
+				<Route exact path="/" render={() => <Home authorized={auth} />} />
+				<Route path="/login" component={LoginPage} />
+				<Route path="/register" component={RegisterPage} />
+				<Route
+					exact
+					path="/todos"
+					component={requireAuth(ReactDom.render = () => (<TodoPage />))}
+				/>
+			</div>
+		</div>
+	);
+}
+
+const styles = () => ({
+	grid: {
+		display: 'grid',
+		fontFamily: 'arial',
+		gridGap: '10px',
+		gridTemplateAreas: `   
+			" navBar " 
+			" messages "  
+			" main " 
+		`,
+		gridTemplateRows: 'auto',
+		marginTop: '50px',
+		padding: '0px 10px 0px 10px',
+	},
+	main: {
+		gridArea: 'main',
+	},
+	messages: {
+		gridArea: 'messages',
+	},
+});
+
+App.propTypes = propTypes;
+App.displayName = 'App';
+
+export default connect(mapStateToProps, mapDispatchToProps)(withStyles(styles, { withTheme: true })(App));
