b1f310226ee4ffa958e6576a643e70aab2a278db
NeuTrix <neutrix@brandgeneering.com>
2018-12-17 12:20:51 -0800

    Update Todo Form rendering

--- src/components/todos/TodoForm.js
+++ src/components/todos/TodoForm.js
@@ -1,85 +1,26 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-//  custom
+import { withStyles } from '@material-ui/core/styles';
 import normalizeDate from '../../helpers/normalizeDate';
 import { colors, media } from '../../helpers/cssConstants';
 
-
-// +++++++++ CSS  +++++++++
-
-const Grid = styled.form`
-	/* mobile view */
-	display: grid;
-	grid-template-areas:
-		" glyph task task task "
-		" . rank date add " 
-	;
-	grid-template-columns: 1fr 3fr 4fr 1fr;
-	grid-gap: 10px;
-
-	background-color: aliceblue;
-	padding: 10px;
-	margin-bottom: 20px;
-
-	& * {
-		font-size: 1em;
-		&:hover {
-			color: steelblue;
-		}
-	}
-
-	/* iPad and large view */
-	@media (${media._medium}) {
-		grid-template-areas: 
-			" ...  	...   ...  	...   ...  "
-			" glyph 	task  rank  date 	add  "
-			" ...  	...   ...  	...   ...  "
-		;
-		grid-template-columns: 1fr 8fr 2fr 3fr 1fr;
-	}
-`;
-
-const Add = styled.button`
-	grid-area: add;
-	color: #00cc00;
-	background: none;
-  border: none;
-	font-size: 2em;
-	`;
-
-const DatePick = styled.input`
-	grid-area: date;
-	text-indent: 10px;
-`;
-
-const Rank = styled.select`grid-area: rank;`;
-
-const Task = styled.input`
-	grid-area: task;
-	text-indent: 10px;
-	&:hover {
-		color: steelblue
-	}
-`;
-
-const Glyph = styled.i`
-	grid-area: glyph;
-	color: ${colors._iceblue} ;
-	font-size: 1.5em;
-`;
-
-// +++++++++ COMPONENT +++++++++
+const propTypes = {
+	createTodo: PropTypes.func.isRequired,
+	owner: PropTypes.string.isRequired,
+};
 
 class TodoForm extends Component {
 
 	constructor(props) {
+		// const { owner } = props;
 		super(props);
 		this.state = {
 			date: normalizeDate(new Date()),
-			task: '',
+			owner: this.props.owner,
+			// owner,
 			rank: 'Med',
-		 	owner: this.props.owner,
+			task: '',
 		};
 		this.handleSubmit = this.handleSubmit.bind(this);
 		this.handleChange = this.handleChange.bind(this);
@@ -97,15 +38,17 @@ class TodoForm extends Component {
 	}
 
 	render() {
+		const { classes } = this.props;
+
 		return (
-			<Grid
-				className={`TodoForm ${this.props.className} 
-				boxClr engrBox`}
+			<div
+				className={classes.grid}
 				onSubmit={this.handleSubmit}
 			>
-				<Glyph className="ctr engr fa fa-tasks fa-lg" />
+				<div className={classes.glyph} />
 
-				<Task
+				<div
+					className={classes.task}
 					id="new_item_task"
 					type="text"
 					name="task"
@@ -116,8 +59,8 @@ class TodoForm extends Component {
 					required
 				/>
 
-				<Rank
-					className="mat"
+				<div
+					className={classes.rank}
 					id="new_item_priority"
 					name="rank"
 					type="select"
@@ -128,10 +71,10 @@ class TodoForm extends Component {
 					<option value="Med">	Med		</option>
 					<option value="Low">	Low		</option>
 
-				</Rank>
+				</div>
 
-				<DatePick
-					className="mat"
+				<div
+					className={classes.datePick}
 					id="new_item_date"
 					name="date"
 					type="date"
@@ -139,27 +82,79 @@ class TodoForm extends Component {
 					value={this.state.date}
 				/>
 
-				<Add
-					className="ctr engr fa fa-plus fa-2x"
+				<div
+					className={classes.add}
 					id="new_item_submit"
 					type="submit"
 				/>
 
-			</Grid>
+			</div>
 		);
 	}
 }
 
-// +++++++++ PROPS +++++++++
+const styles = {
+	grid: {
+		/* mobile view */
+		backgroundColor: 'aliceblue',
+		display: 'grid',
+		gridGap: 10,
+		gridTemplateAreas: `
+			" glyph task task task "
+			" . rank date add " 
+		`,
+		gridTemplateColumns: '1fr 3fr 4fr 1fr',
+		marginBottom: 20,
+		padding: 10,
+		// & * {
+		// 	font-size: 1em,
+		// 	&:hover {
+		// 		color: steelblue,
+		// 	}
+	},
 
-TodoForm.propTypes = {
-	createTodo: PropTypes.func.isRequired,
-	owner: PropTypes.string.isRequired,
-};
+	/* iPad and large view */
+	// @media (${media._medium}) {
+	// 	gridTemplateAreas: 
+	// 		" glyph 	task  rank  date 	add  "
+	// 	,
+	// 	gridTemplateColumns: 1fr 8fr 2fr 3fr 1fr,
+	// },
 
-TodoForm.defaultProps = {
-	createTodo: f => f,
-	owner: 'Default from APP.js',
-};
+	add: {
+		background: 'none',
+		border: 'none',
+		color: '#00cc00',
+		fontSize: '2em',
+		gridArea: 'add',
+	},
 
-export default TodoForm;
+	datePick: {
+		gridArea: 'date',
+		textIndent: 10,
+	},
+
+	glyph: {
+		color: 'aliceblue',
+		fontSize: '1.5em',
+		gridArea: 'glyph',
+	},
+
+	rank: {
+		gridArea: 'rank',
+	},
+
+	task: {
+		gridArea: 'task',
+		textIndent: 10,
+		// &: hover {
+		// color: 'steelblue'
+		// }
+	},
+
+
+}
+
+TodoForm.propTypes = propTypes;
+
+export default withStyles(styles)(TodoForm);
--- src/test/components/TodoForm.test.js
+++ src/test/components/TodoForm.test.js
@@ -1,11 +1,26 @@
 /* eslint-env node, mocha */
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { render } from 'enzyme';
 import TodoForm from '../../components/todos/TodoForm';
 
 it('renders without crashing', () => {
 	const div = document.createElement('div');
 	ReactDOM.render(
-		<TodoForm />, div
+		<TodoForm
+			createTodo={f => f}
+			owner="100"
+		/>, div,
 	);
+});
+
+const wrapper = render(
+	<TodoForm
+		createTodo={f => f}
+		owner="100"
+	/>,
+);
+
+it('...renders the TodoForm ', () => {
+	expect(wrapper).toMatchSnapshot();
 });
