14b78e5192d99bf60ca4d562775667b7de4b953c
NeuTrix <neutrix@brandgeneering.com>
2019-01-06 13:27:47 -0800

    Message animation approximates spec- working
    Still need to tweak the automated fading and other flow

--- src/components/messages/FlashMessage.js
+++ src/components/messages/FlashMessage.js
@@ -3,8 +3,11 @@ import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 import Typography from '@material-ui/core/Typography';
 import CloseIcon from '@material-ui/icons/Close';
+import Slide from '@material-ui/core/Slide';
+import Fade from '@material-ui/core/Fade';
 
 const propTypes = {
+	classes: PropTypes.instanceOf(Object).isRequired,
 	deleteFlashMessage: PropTypes.func.isRequired,
 	message: PropTypes.instanceOf(Object).isRequired,
 };
@@ -13,37 +16,49 @@ class FlashMessage extends Component {
 
 	constructor(props) {
 		super(props);
+		this.state = {
+			show: true,
+		};
 		this.onClick = this.onClick.bind(this);
 		this.closeMessage = this.closeMessage.bind(this);
 	}
 
-	closeMessage() {
-		return this.props.deleteFlashMessage(this.props.message._id);
-	}
-
 	onClick(e) {
 		e.preventDefault();
-		setTimeout(this.closeMessage(), 750);
+		this.closeMessage();
+	}
+
+	closeMessage() {
+		setTimeout(() => {
+			this.setState({ show: false });
+		}, 100);
+		setTimeout(() => {
+			this.props.deleteFlashMessage(this.props.message._id);
+		}, 2000);
 	}
 
 	render() {
 		const { classes, message } = this.props;
-		setTimeout(this.closeMessage, 1000000);
+		const { show } = this.state;
+		setTimeout(() => {
+			this.closeMessage();
+		}, 5000);
 
 		return (
-			< div className = {
-				classes[message.type],
-				classes.grid
-			} >
+			<Fade
+				in={show}
+				timeout={{ enter: 500, exit: 750 }}
+			>
+				<div className={classes.grid}>
 					<Typography
-						className={classes[message.type]} 
-						
+						className={classes[message.type]}
 						variant="body2"
 					>
-						<CloseIcon className={classes.closeIcon}  onClick={this.onClick} />
+						<CloseIcon className={classes.closeIcon} onClick={this.onClick} />
 						{message.text}
 					</Typography>
-			</div>
+				</div>
+			</Fade>
 		);
 	}
 }
@@ -109,52 +124,52 @@ const styles = theme => ({
 	// 	to {opacity: 1,}
 	// }
 
-	// &:hover {
-	// 	backgroundColor: whitesmoke,
-	// 	color: darkgrey,
-	// 	border: 1px solid black,
-	// 	transition: 5s,
+// &:hover {
+// 	backgroundColor: whitesmoke,
+// 	color: darkgrey,
+// 	border: 1px solid black,
+// 	transition: 5s,
 
-	// 	animation: fade-out 5s,
-	// 	@keyframes fade-out {
-	// 		from {opacity: 1.0,}
-	// 		to {opacity: 0.4,}
-	// 	}
-	// },
+// 	animation: fade-out 5s,
+// 	@keyframes fade-out {
+// 		from {opacity: 1.0,}
+// 		to {opacity: 0.4,}
+// 	}
+// },
 
-	// success: {
+// success: {
 
-	// }
+// }
 
-	// ${({ type }) => (type === 'success' ? `
-	// 		background: greenyellow ;
-	// 		color: ${colors._mintgreen} ;
-	// 		borderColor: ${colors._mintgreen} ;
-	// 	` : type === 'error' ? `
-	// 		color: red;
-	// 		borderColor: red;
-	// 		background: ${colors._pinkrose} ;
-	// 	` : type === 'info' ? `
-	// 		color: ${colors._deepblue} ;
-	// 		borderColor: ${colors._deepblue} ;
-	// 		background: aliceblue;
-	// 	` : type === 'warning' ? `
-	// 		color: darkgoldenrod;
-	// 		borderColor: darkgoldenrod;
-	// 		background: lightgoldenrodyellow;
-	// 	` : 'color: grey')
-	// 	},
+// ${({ type }) => (type === 'success' ? `
+// 		background: greenyellow ;
+// 		color: ${colors._mintgreen} ;
+// 		borderColor: ${colors._mintgreen} ;
+// 	` : type === 'error' ? `
+// 		color: red;
+// 		borderColor: red;
+// 		background: ${colors._pinkrose} ;
+// 	` : type === 'info' ? `
+// 		color: ${colors._deepblue} ;
+// 		borderColor: ${colors._deepblue} ;
+// 		background: aliceblue;
+// 	` : type === 'warning' ? `
+// 		color: darkgoldenrod;
+// 		borderColor: darkgoldenrod;
+// 		background: lightgoldenrodyellow;
+// 	` : 'color: grey')
+// 	},
 
-	// delete: {
-	// 	border: 'none',
-	// 	marginRight: theme.spacing.unit,
-	// 	display: 'inherit',
-	// 	fontSize: '1.25em',
-	// 	gridArea: 'delete',
-	// 	height: 'auto',
-	// 	placeContent: 'center',
-	// 	width: 'auto',
-	// },
+// delete: {
+// 	border: 'none',
+// 	marginRight: theme.spacing.unit,
+// 	display: 'inherit',
+// 	fontSize: '1.25em',
+// 	gridArea: 'delete',
+// 	height: 'auto',
+// 	placeContent: 'center',
+// 	width: 'auto',
+// },
 
 FlashMessage.propTypes = propTypes;
 
