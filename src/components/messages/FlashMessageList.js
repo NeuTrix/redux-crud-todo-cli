23ca70fddf07b02177315df753053da7d3a6356f
NeuTrix <neutrix@brandgeneering.com>
2019-01-06 19:23:07 -0800

    Refactor FMsgList as a pure function vs class

--- src/components/messages/FlashMessageList.js
+++ src/components/messages/FlashMessageList.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import FlashMessage from './FlashMessage';
@@ -9,23 +9,21 @@ const propTypes = {
 	messages: PropTypes.instanceOf(Array).isRequired,
 };
 
-class FlashMessagesList extends Component {
+function FlashMessagesList(props) {
+	const { deleteFlashMessage, messages } = props;
+	const messageArray = messages.map(msg => (
+		<FlashMessage
+			key={msg._id}
+			message={msg}
+			deleteFlashMessage={deleteFlashMessage}
+		/>
+	));
 
-	render() {
-		const messages = this.props.messages.map(message => (
-			<FlashMessage
-				key={message._id}
-				message={message}
-				deleteFlashMessage={this.props.deleteFlashMessage}
-			/>
-		));
-
-		return (
-			<div>
-				{ messages.reverse() }
-			</div>
-		);
-	}
+	return (
+		<div>
+			{ messageArray.reverse() }
+		</div>
+	);
 }
 
 FlashMessagesList.propTypes = propTypes;
